fe9ba8de42e21a9e9535660c8bd927f5e1f63bbe
Alex Thomas <athom09@emory.edu>
2013-01-30 11:36:42 -0500

    temp changes to test doc setup

--- voyages/apps/about/models.py
+++ voyages/apps/about/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class C1(mmodels.Model):
+    '''This is the docs for C1'''
+    a = 1
+
+
+    def F1(self):
+        '''Doc for F1'''
+        x=1
+
+def F2():
+    '''Doc for F2'''
+    x=1
--- voyages/apps/about/views.py
+++ voyages/apps/about/views.py
@@ -1,1 +1,5 @@
 # Create your views here.
+
+def V1(self):
+    '''Doc for V1'''
+    x = 3
