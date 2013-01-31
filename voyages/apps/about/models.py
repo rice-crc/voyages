90cad4aa7e2dcaa57b15f0e990a9466b69c938ee
Alex Thomas <athom09@emory.edu>
2013-01-31 01:05:31 -0500

    Removed temp info in models.py and views.py

--- voyages/apps/about/models.py
+++ voyages/apps/about/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class C1(mmodels.Model):
-    '''This is the docs for C1'''
-    a = 1
-
-
-    def F1(self):
-        '''Doc for F1'''
-        x=1
-
-def F2():
-    '''Doc for F2'''
-    x=1
--- voyages/apps/about/views.py
+++ voyages/apps/about/views.py
@@ -1,5 +1,1 @@
 # Create your views here.
-
-def V1(self):
-    '''Doc for V1'''
-    x = 3
