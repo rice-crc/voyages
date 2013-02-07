d15a128edc19fe600e32d9f131b1b7e378e62019
Alex Thomas <athom09@emory.edu>
2013-02-07 13:53:28 -0500

    Configred Static app

--- voyages/settings.py
+++ voyages/settings.py
@@ -29,7 +29,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -40,6 +40,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'sitemedia'),
 )
 
 # List of finder classes that know how to find static files in
