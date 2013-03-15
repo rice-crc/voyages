6bea6c5d2e6b4ce34220aa4c898fd1dffdd31972
voviethoang <voviethoang@gmail.com>
2013-03-15 09:38:32 -0400

    Fixed breadcrumb

--- voyages/sitemedia/scripts/breadcrumb.js
+++ voyages/sitemedia/scripts/breadcrumb.js
@@ -1,13 +1,19 @@
 /* Retrieve the href/path to the current page*/
-
 var href = document.location.href;
 var seperatorst = "&nbsp; <img src=\"/static/images/breadcrumb/breadcrumb-separator.png \" /> ";
 var elem = href.split("/");
 var maxIdx = elem.length - 1;
+
 /* Parse the link to the home page */
 path = "&nbsp;&nbsp;<a href=\"" + href.substring(0, href.indexOf("/" + elem[2]) + elem[2].length + 1) + "/\">" + "Home"  + "</a>";
 
-if (maxIdx >= 3 && elem[3] != "") {
+/* For landing pages of each section */
+if (maxIdx == 3 && elem[3] != "") {
+	path += seperatorst + majorSectionName[elem[3]] + "</a>";
+}
+
+/* For pages inside each section */
+if (maxIdx >= 4 && elem[3] != "") {
 	path += seperatorst + "<a href=\"" + href.substring(0, href.indexOf("/" + elem[3]) + elem[3].length + 1) 
 			+ "/\">" + majorSectionName[elem[3]] + "</a>";
 			
@@ -20,8 +26,7 @@ if (maxIdx >= 3 && elem[3] != "") {
 	
 	if (elem[maxIdx] != "index.html")
 		/* The title of the index page to each section is never included more than once */
-		path += seperatorst + "<a href=\"" + href.substring(0, href.indexOf("/" + elem[maxIdx]) + elem[maxIdx].length + 1)
-			 + "/\">" + document.title+ "</a>";
+		path += seperatorst + document.title;
 }
 /* Output the bread crumb */
 document.writeln(path);
