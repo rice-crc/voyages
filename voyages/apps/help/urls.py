281894829336ae00e9f79aa8215512f082553c2c
voviethoang <voviethoang@gmail.com>
2013-04-01 11:30:45 -0400

    Change topbar font size

--- voyages/apps/help/urls.py
+++ voyages/apps/help/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from voyages.apps.voyage.views import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django.views.generic.simple',
     url(r'^sitemap.html$', 'direct_to_template', {'template': 'help/sitemap.html'}),
     url(r'^faqs.html$', 'direct_to_template', {'template': 'help/faqs.html'}),
     url(r'^demos.html$', 'direct_to_template', {'template': 'help/demos.html'}),
--- voyages/apps/help/views.py
+++ voyages/apps/help/views.py
@@ -1,7 +1,4 @@
 # Create your views here.
-
-# Create your views here.
-
 from django.template import Context, loader
 from django.http import HttpResponse, HttpResponseRedirect, QueryDict
 from django.shortcuts import render_to_response, get_object_or_404
@@ -11,11 +8,6 @@ from django.template.response import TemplateResponse
 from django.utils.http import base36_to_int, is_safe_url
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
-def default(request):
-    # View code here...
-    return render_to_response('default.html',
-                              context_instance=RequestContext(request))
 
 
 
