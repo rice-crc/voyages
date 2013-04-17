96cc3fc1438c9ab6a7228438c630c0c8a43dbcbb
voviethoang <voviethoang@gmail.com>
2013-04-17 12:45:11 -0400

    Fixed help urls.py

--- voyages/apps/help/urls.py
+++ voyages/apps/help/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.defaults import patterns, include, url
 from voyages.apps.voyage.views import *
 
 urlpatterns = patterns('django.views.generic.simple',
-    url(r'^page_sitemap$', 'direct_to_template', {'template': 'help/page_sitemap.html'}),
-    url(r'^page_faqs$', 'direct_to_template', {'template': 'help/page_faqs.html'}),
-    url(r'^page_demos$', 'direct_to_template', {'template': 'help/page_demos.html'}),
-    url(r'^page_glossary$', 'direct_to_template', {'template': 'help/page_glossary.html'}),
-    url(r'^page_legal$', 'direct_to_template', {'template': 'help/page_legal.html'}),
-    url(r'^page_demo-overview$', 'direct_to_template', {'template': 'help/page_demo-overview.html'}),
-    url(r'^page_demo-search$', 'direct_to_template', {'template': 'help/page_demo-search.html'}),
+    url(r'^page_sitemap$', 'direct_to_template', {'template': 'help/page_sitemap.html'}, name='sitemap'),
+    url(r'^page_faqs$', 'direct_to_template', {'template': 'help/page_faqs.html'}, name='faqs'),
+    url(r'^page_demos$', 'direct_to_template', {'template': 'help/page_demos.html'}, name='demos'),
+    url(r'^page_glossary$', 'direct_to_template', {'template': 'help/page_glossary.html'}, name='glossary'),
+    url(r'^page_legal$', 'direct_to_template', {'template': 'help/page_legal.html'}, name='legal'),
+    url(r'^page_demo-overview$', 'direct_to_template', {'template': 'help/page_demo-overview.html'}, name='demo-overview'),
+    url(r'^page_demo-search$', 'direct_to_template', {'template': 'help/page_demo-search.html'}, name='demo-search'),
     
     url(r'^help$', 'direct_to_template', {'template': 'help/helpbase.html'}, name='help'),
 )
