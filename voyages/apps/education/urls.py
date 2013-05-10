61221525ee2d7d408343a2a0773b8569c858469e
voviethoang <voviethoang@gmail.com>
2013-05-10 15:02:00 -0400

    Lesson plans done!

--- voyages/apps/education/urls.py
+++ voyages/apps/education/urls.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
+
 # To be used later
 # from voyages.apps.education.views import *
 
 urlpatterns = patterns('django.views.generic.simple',
     url(r'^$', 'direct_to_template', {'template': 'education/index.html'}, name='index'),
-    url(r'^lesson-plans$', 'direct_to_template', {'template': 'education/lesson-plans.html'}, name='lesson-plans'),
     url(r'^web-resources$', 'direct_to_template', {'template': 'education/others.html'}, name='others'),
+)
+
+urlpatterns += patterns('',
+    url(r'^lesson-plans$', 'voyages.apps.education.views.lessonplan', name='lesson-plans'),                    
 )
--- voyages/apps/education/views.py
+++ voyages/apps/education/views.py
@@ -1,13 +1,30 @@
 # Create your views here.
 from django.http import Http404
-from django.template import TemplateDoesNotExist, Context, loader
-from django.views.generic.simple import direct_to_template
+from django.template import TemplateDoesNotExist, Context, loader, RequestContext
+from django.shortcuts import render_to_response
+from voyages.apps.education.models import *
+from voyages.apps.contribute.models import *
 
 def lessonplan(request):
     # We might want to do some error checking for pagenum here. Even though 404 will be raised if needed
-    lessons = LessonPlan.objects.order_by('order')
-    standards = LessonStandard.objects.all()
+    lessons_all = LessonPlan.objects.order_by('order')
+    standards_all = LessonStandard.objects.all()
     
-    return render_to_response(templatename, {},
-                              context_instance=RequestContext(request, {"lesson_plans" : lessons}));
+    lesson_plan_list = []
+    
+    # Iterate and retrieve all nested submenus
+    for lesson in lessons_all:
+        sub_lesson = []
+        
+        standard_list = standards_all.filter(lesson=lesson.id).order_by('type')
+        standard_type_list = standard_list.values_list('type', flat=True).distinct()
+        
+        for std_type in standard_type_list:
+            text_list = standard_list.filter(type=std_type)
+            sub_lesson.append({'type' : std_type, 'text' : text_list})
+        
+        lesson_plan_list.append({'lesson' : lesson, 'standard' : sub_lesson})
+    
+    return render_to_response('education/lesson-plans.html', {"lesson_plans" : lesson_plan_list},
+                              context_instance=RequestContext(request));
                             
