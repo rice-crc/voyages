ba8b2f0df18bb8e8128936a29a354b9903ac12b2
voviethoang <voviethoang@gmail.com>
2013-05-31 15:18:27 -0400

    Moved admin site hiding Site and Group from urls.py to apps/contribute/admin.py

---
+++ voyages/apps/contribute/admin.py
@@ -0,0 +1,9 @@
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+# Hide the Site and Group features
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group
+admin.site.unregister(Site)
+admin.site.unregister(Group)
