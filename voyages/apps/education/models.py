e3a8ec62e462bf628ef60cb522b5260991af1ae0
voviethoang <voviethoang@gmail.com>
2013-06-03 16:04:42 -0400

    Add documentation to the lesson plan model (in the education app)

--- voyages/apps/education/models.py
+++ voyages/apps/education/models.py
@@ -2,8 +2,12 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 
-# Left menu item
 class LessonPlan(models.Model):
+    """
+    A single lesson plan written by some authors, containing abstract,
+     grade-level, title (known as text attribute), course level, keywords and etc
+
+    """
     text = models.CharField(_('Title'),max_length=100)
     author = models.CharField(_('Author'),max_length=50)
     grade_level = models.CharField(_('Grade Level'),max_length=50)
@@ -21,11 +25,21 @@ class LessonPlan(models.Model):
        return self.text
 
 class LessonStandardType(models.Model):
+    """
+    A type of lesson standards (such as NCHS or NCSS standards)
+
+    """
     type = models.CharField(_('Standard Type'),max_length=100)
     def __unicode__(self):
        return self.type
 
 class LessonStandard(models.Model):
+    """
+    An actual standard or group of standards of a lesson plan
+      belonging to the same lesson standard type
+    related to :model:`education.LessonStandardType`
+    and also to :model:`education.LessonStandard`
+    """
     type = models.ForeignKey(LessonStandardType)
     text = models.CharField(_('Text'),max_length=100)
     lesson = models.ForeignKey(LessonPlan)
@@ -33,8 +47,11 @@ class LessonStandard(models.Model):
        return self.text
 
 class LessonPlanFile(models.Model):
+    """
+    An attached file (presentation or pdf) to a lesson plan
+    related to :model:`education.LessonStandard`
+    """
     file = models.FileField(upload_to='lessonplan')
     filetitle = models.CharField(('File name'),max_length=50)
     lesson = models.ForeignKey(LessonPlan)
-    
     
