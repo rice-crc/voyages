bb0e075afd42798fe4dd31e7d1d92899702d126a
Alex Thomas <athom09@emory.edu>
2013-06-03 15:50:44 -0400

    made Title and Url for flatpages read-only in admin because editing these values would be bad.

--- voyages/apps/voyage/admin.py
+++ voyages/apps/voyage/admin.py
@@ -3,6 +3,8 @@ from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
 
 class FlatPageAdmin(FlatPageAdminOld):
+    list_display = ['title', 'url']
+    readonly_fields= ['title', 'url']
     class Media:
         js = ( 'scripts/tiny_mce/tinymce.min.js',
               'scripts/tiny_mce/textareas.js',
