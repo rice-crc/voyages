d7ee7be60372fcf5c19f73250754a8b3d7cb80e5
voviethoang <voviethoang@gmail.com>
2013-06-06 12:00:00 -0400

    Forgot forms.py in help

---
+++ voyages/apps/help/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.db.models import get_model
+from django.contrib.auth.models import User
+from voyages.apps.help.models import *
+from voyages.extratools import AdvancedEditor
+ 
+class FaqAdminForm(forms.ModelForm):
+    """
+    Form for editing HTML for FAQ answer
+    """
+    question = forms.CharField(widget=forms.Textarea(attrs={'rows':4, 'cols':60}))
+    answer = forms.CharField(widget=AdvancedEditor(attrs={'class' : 'tinymcetextarea'}))
+    question_order = forms.IntegerField()
+
+    class Meta:
+        model = Faq
