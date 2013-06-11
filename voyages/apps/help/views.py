e43093121779d130396d09eb79fdf4b64f957b7b
Tomasz Jurczyk <tjurczy@emory.edu>
2013-06-11 17:03:55 -0400

    Added Glossary search

--- voyages/apps/help/views.py
+++ voyages/apps/help/views.py
@@ -7,35 +7,58 @@ from haystack.forms import HighlightedSearchForm
 
 def glossary_page(request):
     letters = []
-    glossary_content = [];
-    glossary_dict = {};
+    letters_found = {}
+    glossary_content = []
+    glossary_dict = {}
     results = []
     query = ""
-
-    for i in Glossary.objects.all():
-        if i.term[0] not in letters:
-            if glossary_dict:
-                glossary_content.append(glossary_dict);
-            letters.append(i.term[0]);
-            glossary_dict = {};
-            glossary_dict["letter"] = i.term[0];
-            glossary_dict["terms"] = [];
-        glossary_dict["terms"].append({"term": i.term, "description": i.description});
-    glossary_content.append(glossary_dict);
-
-    letters.sort();
 
     if request.method == 'POST':
         form = HighlightedSearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data['q']
-            results = SearchQuerySet().filter(content=query).highlight()
+            results = SearchQuerySet().filter(content=query).models(Glossary)
+            # find letters
+            for i in Glossary.objects.all():
+                if letters_found.get(i.term[0]) == None:
+                    letters_found[i.term[0]] = 0
+                    letters.append(i.term[0])
+            for i in results:
+                # if letter does not exist in found letters
+                if letters_found[i.term[0]] == 0:
+                    letters_found[i.term[0]] = 1
+                    glossary_dict = {};
+                    glossary_dict["letter"] = i.term[0];
+                    glossary_dict["terms"] = [];
+                    glossary_content.append(glossary_dict);
+
+                # add item to proper place
+                for j in glossary_content:
+                    if j["letter"] == i.term[0]:
+                        j["terms"].append({"term": i.term, "description": i.description});
+
+        return render_to_response('help/page_glossary.html', {'glossary': sorted(glossary_content, key=lambda k: k['letter']), 
+            'letters': letters, 'form': form, 'letters_found': sorted(letters_found, key=lambda key: letters_found[key]), 'results': results, 
+                                    'query': query}, context_instance=RequestContext(request))
+
+
     else:
         form = HighlightedSearchForm()
-
-    return render_to_response('help/page_glossary.html', {'letters': letters, 'glossary': glossary_content, 'form': form, 
-                            'results': results, 'query': query},
-                              context_instance=RequestContext(request));
+        for i in Glossary.objects.all():
+            if i.term[0] not in letters:
+                if glossary_dict:
+                    glossary_content.append(glossary_dict);
+                letters.append(i.term[0]);
+                glossary_dict = {};
+                glossary_dict["letter"] = i.term[0];
+                glossary_dict["terms"] = [];
+            glossary_dict["terms"].append({"term": i.term, "description": i.description});
+        glossary_content.append(glossary_dict);
+        letters.sort();
+        #response = "help/page_glossary.html, {'glossary': sorted(glossary_content, key=lambda k: k['letter']), 'letters': letters, 'form': form}, context_instance=RequestContext(request)"
+        return render_to_response('help/page_glossary.html', {'letters': letters, 'glossary': sorted(glossary_content, key=lambda k: k['letter']), 'form': form, 
+                                    'results': results, 'query': query},
+                                    context_instance=RequestContext(request));
 
 
 def get_faqs(request):
