b57d51e4c318743cbd3da59e24a427b9b1d4edbf
voviethoang <voviethoang@gmail.com>
2013-06-12 13:16:22 -0400

    Fixed hyperlink in FAQs (please reload faq_all.json file)

--- voyages/sitemedia/scripts/help/faqs.js
+++ voyages/sitemedia/scripts/help/faqs.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$(".faq-answer a").click(function(ev) {
-		if (this.href.indexOf("/help/help?section=") == -1 && (this.href).indexOf(window.location.hostname) >= 0) {
+		if (this.href.indexOf("/help/") == -1 && (this.href).indexOf(window.location.hostname) >= 0) {
 			/* Website internal links open page in the parent window
 			 * except for Help pages open in the same window */
+			alert("home");
 			ev.preventDefault();
 			openPage($(this).attr("href"));
 		}
