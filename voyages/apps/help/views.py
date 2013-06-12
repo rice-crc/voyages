537da69c4417fa55f2f368e8c57abef3207171fb
Tomasz Jurczyk <tjurczy@emory.edu>
2013-06-12 11:15:12 -0400

    Final glossary page

--- voyages/apps/help/views.py
+++ voyages/apps/help/views.py
@@ -1,46 +1,57 @@
 from django.http import Http404
 from django.template import TemplateDoesNotExist, Context, loader, RequestContext
 from django.shortcuts import render_to_response
+from django.utils.datastructures import SortedDict
 from .models import Glossary, Faq, FaqCategory
 from haystack.query import SearchQuerySet
 from haystack.forms import HighlightedSearchForm
 
 def glossary_page(request):
+    """
+    Display the entire glossary page
+    containing glossary entries from :model:`help.Glossary`
+    """
+
     letters = []
-    letters_found = {}
     glossary_content = []
     glossary_dict = {}
-    results = []
-    query = ""
 
     if request.method == 'POST':
+        letters_found = SortedDict()
+        results = []
+        query = ""
+
         form = HighlightedSearchForm(request.POST)
+
         if form.is_valid():
+            # Perform the query
             query = form.cleaned_data['q']
             results = SearchQuerySet().filter(content=query).models(Glossary)
-            # find letters
+
+            # Gather all letters
             for i in Glossary.objects.all():
                 if letters_found.get(i.term[0]) == None:
                     letters_found[i.term[0]] = 0
                     letters.append(i.term[0])
+
+            # Collect results
             for i in results:
-                # if letter does not exist in found letters
-                if letters_found[i.term[0]] == 0:
-                    letters_found[i.term[0]] = 1
+                # If letter does not exist in found letters
+                if letters_found[i.glossary_term[0]] == 0:
+                    letters_found[i.glossary_term[0]] = 1
                     glossary_dict = {};
-                    glossary_dict["letter"] = i.term[0];
+                    glossary_dict["letter"] = i.glossary_term[0];
                     glossary_dict["terms"] = [];
                     glossary_content.append(glossary_dict);
 
-                # add item to proper place
+                # Add item to proper letter
                 for j in glossary_content:
-                    if j["letter"] == i.term[0]:
-                        j["terms"].append({"term": i.term, "description": i.description});
+                    if j["letter"] == i.glossary_term[0]:
+                        j["terms"].append({"term": i.glossary_term, "description": i.glossary_description});
 
         return render_to_response('help/page_glossary.html', {'glossary': sorted(glossary_content, key=lambda k: k['letter']), 
-            'letters': letters, 'form': form, 'letters_found': sorted(letters_found, key=lambda key: letters_found[key]), 'results': results, 
+            'letters': letters, 'form': form, 'letters_found': letters_found, 'results': results, 
                                     'query': query}, context_instance=RequestContext(request))
-
 
     else:
         form = HighlightedSearchForm()
@@ -54,10 +65,10 @@ def glossary_page(request):
                 glossary_dict["terms"] = [];
             glossary_dict["terms"].append({"term": i.term, "description": i.description});
         glossary_content.append(glossary_dict);
+
         letters.sort();
-        #response = "help/page_glossary.html, {'glossary': sorted(glossary_content, key=lambda k: k['letter']), 'letters': letters, 'form': form}, context_instance=RequestContext(request)"
-        return render_to_response('help/page_glossary.html', {'letters': letters, 'glossary': sorted(glossary_content, key=lambda k: k['letter']), 'form': form, 
-                                    'results': results, 'query': query},
+        return render_to_response('help/page_glossary.html', {'letters': letters, 
+                                    'glossary': sorted(glossary_content, key=lambda k: k['letter']), 'form': form,},
                                     context_instance=RequestContext(request));
 
 def get_faqs(request):
