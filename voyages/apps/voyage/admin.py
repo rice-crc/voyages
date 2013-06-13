4503f20ab4b8ef97c3e156566ee5aafc1a6d963f
voviethoang <voviethoang@gmail.com>
2013-06-13 16:35:24 -0400

    Lesson plan and download pages open ppt and pdf links in a new window

--- voyages/apps/voyage/admin.py
+++ voyages/apps/voyage/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
-from django.contrib.flatpages.admin import FlatpageForm
-from django import forms
-
-class MyFlatAdminForm(FlatpageForm):
-    class Meta:
-        model = FlatPage
 
 class FlatPageAdmin(admin.ModelAdmin):
     fields = ('url', 'title', 'content')
--- voyages/sitemedia/scripts/help/faqs.js
+++ voyages/sitemedia/scripts/help/faqs.js
@@ -1,9 +1,9 @@
+/* Open FAQ link in the parent window */
 $(document).ready(function() {
 	$(".faq-answer a").click(function(ev) {
 		if (this.href.indexOf("/help/") == -1 && (this.href).indexOf(window.location.hostname) >= 0) {
 			/* Website internal links open page in the parent window
 			 * except for Help pages open in the same window */
-			alert("home");
 			ev.preventDefault();
 			openPage($(this).attr("href"));
 		}
