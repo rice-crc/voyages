d50f81ddf431ce5287b67062b6ada459664f8b95
Tomasz Jurczyk <tjurczy@emory.edu>
2013-06-14 11:51:35 -0400

    Working on migrate

--- voyages/apps/voyage/urls.py
+++ voyages/apps/voyage/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import *
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 
 urlpatterns = patterns('',
     url(r'^c(?P<chapternum>\d{2})_s(?P<sectionnum>\d{2})_p(?P<pagenum>\d{2})$','voyages.apps.voyage.views.get_page', name='get-page'),
 )
 
-urlpatterns += patterns('django.views.generic.TemplateView',
+urlpatterns += patterns('',
     url(r'^$', TemplateView.as_view(template_name='voyage/index.html'), name='index'),
     url(r'^understanding-db$', TemplateView.as_view(template_name='voyage/c01_base.html'), name='guide'),
     
@@ -16,7 +16,7 @@ urlpatterns += patterns('django.views.generic.TemplateView',
     
     url(r'^search$', TemplateView.as_view(template_name='under_constr.html'), name='search'),
 
-    url(r'^contribute$', 'redirect_to', {'url':'/contribute'}, name='submission-login'),
+    url(r'^contribute$', RedirectView.as_view(url='/contribute'), name='submission-login'),
     
     url(r'^voyage$', TemplateView.as_view(template_name='under_constr.html'), name='voyage'),
 )
