bb11ac4981ea3e2692bd0fb0f95aa661ff0f6f0e
voviethoang <voviethoang@gmail.com>
2013-06-14 11:03:27 -0400

    Pushed again the CSS

--- voyages/apps/education/tests.py
+++ voyages/apps/education/tests.py
@@ -40,7 +40,20 @@ class TestAllData(TestCase):
             self.assertContains(response, std_type_item.type)
         for lessonstandard_item in LessonStandard.objects.all():
             self.assertContains(response, lessonstandard_item.text)
-        
+
+@override_settings(LANGUAGE_CODE='en')    
+class TestNoData(TestCase):
+    """
+    Simple test on an empty set of lesson plans
+    (no fixture loaded)
+    """
+    def test_empty_lessonplan(self):
+        response = self.client.get(reverse('education:lesson-plans'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Abstract")
+        self.assertNotContains(response, "Author")
+        self.assertNotContains(response, "Course")
+
 @override_settings(LANGUAGE_CODE='en')    
 class TestSmallerSubset(TestCase):
     """
@@ -100,4 +113,68 @@ class TestSmallerSubset(TestCase):
             self.assertNotContains(response, new_lessonplan.grade_level)
             self.assertNotContains(response, new_lessonplan.course)
             self.assertNotContains(response, new_lessonplan.key_words)
+            
+    def test_adding_multiple_lessonplans(self):
+        """
+        Test adding multiple lesson plans to the page
+        and count whether they are added
+        """
+         # Check number of objects
+        initial_count = LessonPlan.objects.count()
+        for i in range(0, 10):
+            sample_text = "test_text_" + str(random.randint(0, 1000000))
+            sample_author = "test_author_" + str(random.randint(0, 1000000))
+            sample_grade_level = "test_grade_" + str(random.randint(0, 10000000))
+            sample_course = "test_course_" + str(random.randint(100000, 1000000))
+            sample_key_words = "test_keywords" + str(random.randint(100000, 1000000))
+            sample_order = str(random.randint(40, 100))
+            sample_abstract = "test_abstract_" + str(random.randint(0, 10000000))
+            
+            new_lessonplan = LessonPlan.objects.create(text=sample_text, author=sample_author, grade_level=sample_grade_level,
+                    course=sample_course, key_words=sample_key_words, order=sample_order, abstract=sample_abstract)
+            
+            # Check if the response contains the lesson plan
+            response = self.client.get(reverse('education:lesson-plans'))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, new_lessonplan.text)
+            self.assertContains(response, new_lessonplan.author)
+            self.assertContains(response, new_lessonplan.grade_level)
+            self.assertContains(response, new_lessonplan.course)
+            self.assertContains(response, new_lessonplan.key_words)
+        self.assertEqual(LessonPlan.objects.count(), initial_count+ 10)
         
+@override_settings(LANGUAGE_CODE='en')    
+class TestLessonStandard(TestCase):
+    """
+    Simple test on a subset of the lesson plan data
+    """
+    fixtures = ['help_lessonplan_data_part1.json']
+    def test_adding_standard(self):
+        """
+        Test simple rendering of the page
+        """
+        std_item_text = "test_text_" + str(random.randint(0, 1000000))
+        std_item_type = LessonStandardType.objects.order_by('?')[0]
+        std_item_lesson = LessonPlan.objects.order_by('?')[0]
+        
+        # Generate a random Lesson Standard
+        std_item = LessonStandard.objects.create(text=std_item_text, type=std_item_type,lesson=std_item_lesson)
+        
+        response = self.client.get(reverse('education:lesson-plans'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, std_item_text)
+        
+        textList = []
+        for i in range(0, 10):
+            std_item_text = "test_text_" + str(random.randint(0, 1000000))
+            std_item_type = LessonStandardType.objects.order_by('?')[0]
+            std_item_lesson = LessonPlan.objects.order_by('?')[0]
+            textList.append(std_item_text)
+            
+            # Generate a random Lesson Standard
+            std_item = LessonStandard.objects.create(text=std_item_text, type=std_item_type,lesson=std_item_lesson)
+        
+        response = self.client.get(reverse('education:lesson-plans'))
+        self.assertEqual(response.status_code, 200)
+        for text_item in textList:
+            self.assertContains(response, text_item)
--- voyages/apps/help/tests.py
+++ voyages/apps/help/tests.py
@@ -3,8 +3,10 @@
 from django.test import TestCase
 from django.test.utils import override_settings
 from django.core.management import call_command
+from django.core.urlresolvers import reverse
 from sys import stderr
 from .models import Glossary, Faq, FaqCategory
+
 
 @override_settings(LANGUAGE_CODE='en')
 class GlossaryEmptyTest(TestCase):
@@ -112,3 +114,101 @@ class GlossaryModifiedTest(TestCase):
 
     def test_search(self):
         pass
+#
+# Test for the FAQ model
+#
+@override_settings(LANGUAGE_CODE='en')
+class TestEmptyFaq(TestCase):
+    """
+    Test when FAQ contains no record
+    """
+    def test_rendering_response_code(self):
+        """
+        Test if initial website is rendering properly
+        """
+
+        # Check response code
+        response = self.client.get(reverse('help:page_faq'))
+        self.assertEqual(response.status_code, 200)
+
+@override_settings(LANGUAGE_CODE='en')
+class TestFaqAllData(TestCase):
+    """
+    Tests on the full dataset of FAQ
+    """
+    fixtures = ['faq_data_all.json']
+
+    def test_rendering_response_code(self):
+        """
+        Test if initial website is rendering properly
+        """
+        # Check response code
+        response = self.client.get(reverse('help:page_faq'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contain_words(self):
+        """
+        Test if glossary website contains all initial data
+        """
+        response = self.client.get('/help/page_glossary')
+        for i in Glossary.objects.all():
+            self.assertEqual(response, i.term)
+            self.assertEqual(response, i.description)
+
+
+@override_settings(LANGUAGE_CODE='en')
+class GlossaryModifiedTest(TestCase):
+    """
+    Tests for modified data from glossary
+    """
+    fixtures = ['glossary_tests.json']
+    initial_objects = 0
+
+    def setUp(self):
+        self.initial_objects = Glossary.objects.count()
+
+    def test_rendering_response_code(self):
+        """
+        Test if initial website is rendering properly
+        """
+
+        # Check response code
+        response = self.client.get('/help/page_glossary')
+        self.assertEqual(response.status_code, 200)
+        print
+
+    def test_glossary_content(self):
+        """
+        Test if glossary website contains all initial data
+        """
+
+        response = self.client.get('/help/page_glossary')
+        for i in Glossary.objects.all():
+            self.assertContains(response, i.term)
+            self.assertContains(response, i.description)
+
+    def test_adding_items(self):
+        """
+        Test adding glossary items (adding and response)
+        """
+        # Add two test items
+        Glossary.objects.create(term="Albariv", description="His explen prougg tring thience Barce, haffer).")
+        Glossary.objects.create(term="Wgsfew", description="Homarro palium was ques: R. Effic fros ru nonamba soccom men unater par?")
+
+        # Check number of objects
+        self.assertEqual(Glossary.objects.count(), self.initial_objects + 2)
+
+        # Check added items
+        self.assertEqual(Glossary.objects.get(pk=self.initial_objects+1).term, "Albariv")
+        self.assertEqual(Glossary.objects.get(pk=self.initial_objects+1).description, "His explen prougg tring thience Barce, haffer).")
+        self.assertEqual(Glossary.objects.get(pk=self.initial_objects+2).term, "Wgsfew")
+        self.assertEqual(Glossary.objects.get(pk=self.initial_objects+2).description, "Homarro palium was ques: R. Effic fros ru nonamba soccom men unater par?")
+
+        response = self.client.get('/help/page_glossary')
+        for i in [Glossary.objects.get(pk=self.initial_objects+1), Glossary.objects.get(pk=self.initial_objects+2)]:
+            self.assertContains(response, i.term)
+            self.assertContains(response, i.description)
+
+        # Check response code
+        response = self.client.get('/help/page_glossary')
+        self.assertEqual(response.status_code, 200)
