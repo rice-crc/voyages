aca5d3a196da61d73f383aecf780dbedb063e954
voviethoang <voviethoang@gmail.com>
2013-06-17 15:22:11 -0400

    Change solr indexing fields from text to string

--- voyages/apps/help/search_indexes.py
+++ voyages/apps/help/search_indexes.py
@@ -5,10 +5,10 @@ from .models import *
 
 class FaqIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    faq_question = indexes.CharField(model_attr='question')
-    faq_answer = indexes.CharField(model_attr='answer')
+    faq_question = indexes.FacetMultiValueField()
+    faq_answer = indexes.FacetMultiValueField()
     faq_question_order = indexes.IntegerField(model_attr='question_order')
-    faq_category_desc = indexes.CharField()
+    faq_category_desc = indexes.FacetMultiValueField()
     faq_category_order = indexes.IntegerField()
 
     def get_model(self):
@@ -17,16 +17,24 @@ class FaqIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+    
+    def prepare_faq_question(self, obj):
+        return obj.question
+    
+    def prepare_faq_answer(self, obj):
+        return obj.answer
+    
     def prepare_faq_category_order(self, obj):
         return obj.category.type_order
+    
     def prepare_faq_category_desc(self, obj):
         return obj.category.text
     
 
 class GlossaryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    glossary_term = indexes.CharField(model_attr='term')
-    glossary_description = indexes.CharField(model_attr='description')
+    glossary_term = indexes.FacetMultiValueField()
+    glossary_description = indexes.FacetMultiValueField()
 
     def get_model(self):
         return Glossary
@@ -34,3 +42,9 @@ class GlossaryIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+
+    def prepare_glossary_term(self, obj):
+        return obj.term
+    
+    def prepare_glossary_description(self, obj):
+        return obj.description
--- voyages/apps/help/views.py
+++ voyages/apps/help/views.py
@@ -4,6 +4,8 @@ from django.utils.datastructures import SortedDict
 from .models import Glossary, Faq, FaqCategory
 from haystack.query import SearchQuerySet
 from haystack.forms import HighlightedSearchForm
+from django.conf import settings
+import string
 
 def glossary_page(request):
     """
@@ -19,65 +21,88 @@ def glossary_page(request):
     ** Template **
     :template:`help/page_glossary.html`
     """
+    
+    def sort_dict(dict):
+        """
+        Sort the dictionary if the dictionary is not empty
+        """
+        try:
+            return sorted(dict, key=lambda k: k['letter'])
+        except:
+            return dict
+        
+    def getsortedresults(qresult):
+        """
+        Sort the result into categories and questions from response returned by the backend engine
+        """
+        glossary_content = []
+        current_item = None
+        count = 0
+        letters = []
+        letters_found = {}
+        
+        for i in string.ascii_uppercase:
+            letters.append(i)
+            letters_found[i] = 0
+        
+        if qresult:
+            # Process results
+            prev_letter = None
+            groupedList = []
+                    
+            for search_result_obj in qresult:
+                current_item = search_result_obj.get_stored_fields()
+                
+                print "%s  %s" % (current_item['glossary_term'][0], current_item['glossary_term'],)
+                
+                if prev_letter is None:
+                    # Update the first letter
+                    prev_letter = current_item['glossary_term'][0]
+                else:
+                    if prev_letter != current_item['glossary_term'][0]:
+                        # Starts a new group of terms
+                        # Add the previous group to glossary_content
+                        glossary_content.append({'letter' : prev_letter, 'terms' : groupedList })
+                        letters_found[prev_letter] = 1
+                            
+                        prev_letter = current_item['glossary_term'][0]
+                        groupedList = []
+                        
+                # Add the question to the grouped list
+                groupedList.append({'term' : current_item['glossary_term'], 'description' : current_item['glossary_description'] })
+            
+            # Add the last result group
+            letters_found[prev_letter] = 1
+            glossary_content.append({'letter' : prev_letter, 'terms' : groupedList })
+            
+        return letters, letters_found, glossary_content
+    
     letters = []
+    letters_found = {}
     glossary_content = []
-    glossary_dict = {}
+    results = []
+    query = ""
 
     if request.method == 'POST':
-        letters_found = SortedDict()
-        results = []
-        query = ""
-
         form = HighlightedSearchForm(request.POST)
 
         if form.is_valid():
             # Perform the query
             query = form.cleaned_data['q']
-            results = SearchQuerySet().filter(content=query).models(Glossary)
-
-            # Gather all letters
-            for i in Glossary.objects.all():
-                if letters_found.get(i.term[0]) == None:
-                    letters_found[i.term[0]] = 0
-                    letters.append(i.term[0])
-
-            # Collect results
-            for i in results:
-                # If letter does not exist in found letters
-                if letters_found[i.glossary_term[0]] == 0:
-                    letters_found[i.glossary_term[0]] = 1
-                    glossary_dict = {};
-                    glossary_dict["letter"] = i.glossary_term[0];
-                    glossary_dict["terms"] = [];
-                    glossary_content.append(glossary_dict);
-
-                # Add item to proper letter
-                for j in glossary_content:
-                    if j["letter"] == i.glossary_term[0]:
-                        j["terms"].append({"term": i.glossary_term, "description": i.glossary_description});
-
-        return render_to_response('help/page_glossary.html', {'glossary': sort_dict(glossary_content),
-            'letters': letters, 'form': form, 'letters_found': letters_found, 'results': results, 
-                                    'query': query}, context_instance=RequestContext(request))
-
+            results = SearchQuerySet().filter(content=query).models(Glossary).order_by('glossary_term')
+            
+      
     else:
         form = HighlightedSearchForm()
-        for i in Glossary.objects.all():
-            if i.term[0] not in letters:
-                if glossary_dict:
-                    glossary_content.append(glossary_dict);
-                letters.append(i.term[0]);
-                glossary_dict = {};
-                glossary_dict["letter"] = i.term[0];
-                glossary_dict["terms"] = [];
-            glossary_dict["terms"].append({"term": i.term, "description": i.description});
-        glossary_content.append(glossary_dict);
-
-        letters.sort();
-
-        return render_to_response('help/page_glossary.html', {'letters': letters, 
-                                    'glossary': sort_dict(glossary_content), 'form': form,},
-                                    context_instance=RequestContext(request));
+        results = SearchQuerySet().models(Glossary).order_by('glossary_term')
+        3/0
+    
+    letters, letters_found, glossary_content = getsortedresults(results)
+    sort_dict(glossary_content)
+    
+    return render_to_response('help/page_glossary.html', {'glossary': sort_dict(glossary_content),
+            'letters': letters, 'form': form, 'letters_found': letters_found, 'results': results, 
+                                    'query': query}, context_instance=RequestContext(request))
 
 
 def get_faqs(request):
@@ -94,8 +119,43 @@ def get_faqs(request):
     ** Template **
     :template:`help/page_faqs.html`
     """
-    count = 0;
+    def getsortedresults(qresult):
+        """
+        Sort the result into categories and questions from response returned by the backend engine
+        """
+        faq_list = []
+        current_item = None
+        count = 0
+        
+        if qresult:
+            # Process results
+            prev_obj = None
+            groupedList = []
+                    
+            for search_result_obj in qresult:
+                current_item = search_result_obj.get_stored_fields()
+                if prev_obj is None:
+                    prev_obj = current_item
+                else:
+                    if prev_obj['faq_category_desc'] == current_item['faq_category_desc']:
+                       # Questions belong to the same category
+                        prev_obj = current_item
+                    else:
+                        # Starts a new group of question (different category)
+                        # Add the previous group to faq_list
+                        faq_list.append({ 'qorder' : count, 'text' : prev_obj['faq_category_desc'], 'questions' : groupedList })
+                        count += 1
+                        prev_obj = current_item
+                        groupedList = []
+                        # Add the question to the grouped list
+                groupedList.append({ 'question': current_item['faq_question'], 'answer' : current_item['faq_answer']})
+            # Add the last result group
+            faq_list.append({ 'qorder' : count, 'text' : prev_obj['faq_category_desc'], 'questions' : groupedList })
+        return faq_list
+    
     faq_list = []
+    qresult = []
+    current_query = ''
     
     if request.method == 'POST':
         # Convert the posted data into Haystack search form
@@ -104,53 +164,12 @@ def get_faqs(request):
             # Perform the query by specifying the search term and sort orders (category and then questions)
             current_query = form.cleaned_data['q']
             qresult = SearchQuerySet().filter(content=current_query).models(Faq).order_by('faq_category_order', 'faq_question_order')
-            
-            current_item = None
-            if qresult:
-                # Process results
-                prev_obj = None
-                groupedList = []
-                
-                for search_result_obj in qresult:
-                    current_item = search_result_obj.get_stored_fields()
-                    if prev_obj is None:
-                        prev_obj = current_item
-                        
-                    else:
-                        if prev_obj['faq_category_desc'] == current_item['faq_category_desc']:
-                            # Questions belong to the same category
-                            prev_obj = current_item
-                        else:
-                             # Starts a new group of question (different category)
-                             # Add the previous group to faq_list
-                             faq_list.append({ 'qorder' : count, 'text' : prev_obj['faq_category_desc'], 'questions' : groupedList })
-                             count += 1
-                             prev_obj = current_item
-                             groupedList = []
-                    # Add the question to the grouped list
-                    groupedList.append({ 'question': current_item['faq_question'], 'answer' : current_item['faq_answer']})
-                    # Add the last result group
-                faq_list.append({ 'qorder' : count, 'text' : prev_obj['faq_category_desc'], 'questions' : groupedList })
      
-        return render_to_response('help/page_faqs.html', {'form' : form, "faq_list" : faq_list, 'current_query' : current_query},
-                              context_instance=RequestContext(request));
     else:
         # return the form if there is no form and display the entire faq (from the database)
         form = HighlightedSearchForm()
-        for faq_cat in FaqCategory.objects.order_by('type_order'):
-            groupedList = Faq.objects.filter(category=faq_cat)
-            if groupedList:
-                # Hides category with no questions
-                faq_list.append({ 'qorder' : count, 'text' : faq_cat.text, 'questions' : groupedList})
-                count += 1
-        return render_to_response('help/page_faqs.html', {'form' : form, "faq_list" : faq_list,},
+        qresult = SearchQuerySet().models(Faq).order_by('faq_category_order', 'faq_question_order')
+    
+    faq_list = getsortedresults(qresult)
+    return render_to_response('help/page_faqs.html', {'form' : form, "faq_list" : faq_list, 'current_query' : current_query},
                               context_instance=RequestContext(request));
-
-def sort_dict(dict):
-    """
-    Sort the dictionary if the dictionary is not empty
-    """
-    try:
-        return sorted(dict, key=lambda k: k['letter'])
-    except:
-        return dict
