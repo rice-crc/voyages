5c71075c09945ad82f0a8f91b53aa35541093542
Tomasz Jurczyk <tjurczy@emory.edu>
2013-06-18 17:08:02 -0400

    VoyageItinerary and other

--- voyages/apps/voyage/models.py
+++ voyages/apps/voyage/models.py
@@ -14,7 +14,8 @@ class Voyage(models.Model):
 
     class SpecificRegion(models.Model):
         """
-        Specific Regions (Countries or Colonies).
+        Specific Regions (countries or colonies).
+        related to: :model:`voyages.apps.voyages.Voyage.BroadRegion`
         """
 
         specific_region = models.CharField("Specific region (country or colony",
@@ -22,6 +23,11 @@ class Voyage(models.Model):
         broad = models.ForeignKey(Voyage.BroadRegion)
 
     class Place(models.Model):
+        """
+        Place (port or location).
+        related to: :model:`voyages.apps.voyages.Voyage.BroadRegion`
+        related to: :model:`voyages.apps.voyages.Voyage.SpecificRegion`
+        """
         place_name = models.CharField(max_length=35)
         broad_region = models.ForeignKey(Voyage.BroadRegion)
         specific_region = models.ForeignKey(Voyage.SpecificRegion)
@@ -35,12 +41,15 @@ class Voyage(models.Model):
     class VoyageShip(models.Model):
         """
         Information about voyage ship.
+        related to: :model:`voyages.apps.voyages.Voyage.SpecificRegion`
+        related to: :model:`voyages.apps.voyages.Voyage.Place`
+
         """
         class VoyageVentureOwner(models.Model):
             """
             Information about other owners.
             """
-            number_of_owner = models.IntegerField(max_length=2)
+            number_of_owner = models.IntegerField(max_length=2, blank=True)
             name_of_owner = models.CharField(max_length=40)
 
         class NationalityOfShip(models.Model):
@@ -48,6 +57,14 @@ class Voyage(models.Model):
             Nationalities of ships.
             """
             nationality_of_ship = models.CharField(max_length=35)
+
+        class ImputedCountryShip(models.Model):
+            """
+            Imputed country in which ship registered.
+            """
+            imputed_country = models.CharField("Imputed country "
+                                               "in which ship registered.",
+                                               max_length=35)
 
         class TonType(models.Model):
             """
@@ -61,21 +78,31 @@ class Voyage(models.Model):
             """
             rig_of_vessel = models.CharField(max_length=25)
 
-
+        # Data variables
         ship_name = models.CharField("Name of vessel", max_length=60)
-        national = models.ForeignKey(NationalityOfShip)
-        tonnage = models.IntegerField("Tonnage of vessel", max_length=4)
+        nationality = models.ForeignKey(NationalityOfShip)
+        tonnage = models.IntegerField("Tonnage of vessel", max_length=4,
+                                       blank=True)
         ton_type = models.ForeignKey(TonType)
         rig_of_vessel = models.ForeignKey(RigOfVessel)
-        guns_mounted = models.IntegerField("Guns mounted", max_length=2)
-        year_of_constr = models.DateField("Year of vessel's construction")
-        #vessel_constr_place =
-        #vessel_constr_region =
-        year_of_reg = models.DateField("Year of vessel's registration")
-        #reg_place =
-        #reg_region =
+        guns_mounted = models.IntegerField("Guns mounted", max_length=2,
+                                            blank=True)
+        year_of_construction = models.DateField("Year of vessel's construction")
+        vessel_construction_place = models.ForeignKey(Voyage.Place)
+        vessel_construction_region = models.ForeignKey(Voyage.SpecificRegion)
+        registered_year = models.DateField("Year of vessel's registration")
+        registered_place = models.ForeignKey(Voyage.Place)
+        registered_region = models.ForeignKey(Voyage.SpecificRegion)
         owner_of_venture = models.CharField("First owner of venture", max_length=60)
         owners = models.ForeignKey(VoyageVentureOwner)
+
+        # Imputed variables
+        imputed_nationality = models.ForeignKey(ImputedCountryShip)
+        tonnage_mod = models.DecimalField("Tonnage standardized on British"
+                                          "measured tons, 1773-1835",
+                                          max_digits=8,
+                                          decimal_places=2,
+                                          blank=True)
 
 
     class VoyageOutcome(models.Model):
@@ -95,11 +122,11 @@ class Voyage(models.Model):
             """
             slaves_outcome = models.CharField("Outcome label", max_length=70)
 
-        class VesselCapturesOutcome(models.Model):
+        class VesselCapturedOutcome(models.Model):
             """
             Outcome of voyage if vessel captured.
             """
-            vessel_captures_outcome = models.CharField("Outcome label",
+            vessel_captured_outcome = models.CharField("Outcome label",
                                                        max_length=35)
 
         class OwnerOutcome(models.Model):
@@ -114,19 +141,80 @@ class Voyage(models.Model):
             """
             resistance_name = models.CharField("Resistance label", max_length=35)
 
-
+        # Data variables
         particular_outcome = models.ForeignKey(ParticularOutcome)
-        outcome_slaves = models.ForeignKey(SlavesOutcome)
-        vessel_captures_outcome = models.ForeignKey(VesselCapturesOutcome)
-        outcome_owner = models.ForeignKey(OwnerOutcome)
         resistance = models.ForeignKey(Resistance)
 
+        # Imputed variables
+        outcome_slaves = models.ForeignKey(SlavesOutcome)
+        vessel_captured_outcome = models.ForeignKey(VesselCapturedOutcome)
+        outcome_owner = models.ForeignKey(OwnerOutcome)
 
 
     class VoyageItinerary(models.Model):
         """
         Voyage Itinerary data.
+        related to: :model:`voyages.apps.voyages.Voyage.BroadRegion`
+        related to: :model:`voyages.apps.voyages.Voyage.SpecificRegion`
+        related to: :model:`voyages.apps.voyages.Voyage.Place`
         """
+
+        # Data variables
+        port_of_departure = models.ForeignKey(Voyage.Place)
+        # Intended variables
+        int_first_port_emb = models.ForeignKey(Voyage.Place)
+        int_second_port_emb = models.ForeignKey(Voyage.Place)
+        int_first_region_purchase_slaves = models.ForeignKey(Voyage.SpecificRegion)
+        int_second_region_purchase_slaves = models.ForeignKey(Voyage.SpecificRegion)
+        int_first_port_dis = models.ForeignKey(Voyage.Place)
+        int_second_port_dis = models.ForeignKey(Voyage.Place)
+        int_first_region_slave_landing = models.ForeignKey(Voyage.SpecificRegion)
+        int_second_region_slave_landing = models.ForeignKey(Voyage.SpecificRegion)
+        # End of intended variables
+        ports_called_buying_slaves = models.IntegerField("Number of ports "
+                                                         "of call prior "
+                                                         "to buying slaves",
+                                                         max_length=3,
+                                                        blank=True)
+
+        first_place_slave_purchase = models.ForeignKey(Voyage.Place)
+        second_place_slave_purchase = models.ForeignKey(Voyage.Place)
+        third_place_slave_purchase = models.ForeignKey(Voyage.Place)
+
+        first_region_slave_emb = models.ForeignKey(Voyage.SpecificRegion)
+        second_region_slave_emb = models.ForeignKey(Voyage.SpecificRegion)
+        third_region_slave_emb = models.ForeignKey(Voyage.SpecificRegion)
+
+        port_of_call_before_atlcrossing = models.ForeignKey(Voyage.Place)
+        number_of_ports_of_call = models.ForeignKey(Voyage.Place)
+
+        first_landing_place = models.ForeignKey(Voyage.Place)
+        second_landing_place = models.ForeignKey(Voyage.Place)
+        third_landing_place = models.ForeignKey(Voyage.Place)
+
+        first_landing_region = models.ForeignKey(Voyage.SpecificRegion)
+        second_landing_region = models.ForeignKey(Voyage.SpecificRegion)
+        third_landing_region = models.ForeignKey(Voyage.SpecificRegion)
+
+        place_voyage_ended = models.ForeignKey(Voyage.Place)
+        region_of_return = models.ForeignKey(Voyage.SpecificRegion)
+        broad_region_of_return = models.ForeignKey(Voyage.BroadRegion)
+
+        # Imputed variables
+        imp_port_voyage_begin = models.ForeignKey(Voyage.Place)
+        imp_region_voyage_begin = models.ForeignKey(Voyage.SpecificRegion)
+        imp_broad_region_voyage_begin = models.ForeignKey(Voyage.BroadRegion)
+        principal_place_of_slave_purchase = models.ForeignKey(Voyage.Place)
+        imp_principal_place_of_slave_purchase = models.ForeignKey(Voyage.Place)
+        imp_principal_region_of_slave_purchase = \
+            models.ForeignKey(Voyage.SpecificRegion)
+        imp_broad_region_of_slave_purchase = \
+            models.ForeignKey(Voyage.BroadRegion)
+        principal_port_of_slave_dis = models.ForeignKey(Voyage.Place)
+        imp_principal_port_slave_dis = models.ForeignKey(Voyage.Place)
+        imp_principal_region_slave_dis = models.ForeignKey(Voyage.SpecificRegion)
+        imp_broad_region_slave_dis = models.ForeignKey(Voyage.BroadRegion)
+
 
     class VoyageDates(models.Model):
         """
@@ -154,6 +242,7 @@ class Voyage(models.Model):
         """
 
     voyage_id = models.AutoField(primary_key=True)
-    voyage_in_cdrom = models.IntegerField("Voyage in 1999 CD-ROM", max_length=1)
+    voyage_in_cdrom = models.IntegerField("Voyage in 1999 CD-ROM", max_length=1,
+                                           blank=True)
     voyage_groupings = models.ForeignKey(VoyageGroupings)
     voyage_outcome = models.ForeignKey(VoyageShip)
