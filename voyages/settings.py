ceb91c63483aec8aa32241c3af1d613249f0ec4b
Tomasz Jurczyk <tjurczy@emory.edu>
2013-07-08 09:59:36 -0400

    Deleted unnecessary code.

--- voyages/settings.py
+++ voyages/settings.py
@@ -156,12 +156,6 @@ SOUTH_TESTS_MIGRATE = False
 LOGIN_URL = '/contribute/login/'
 LOGIN_REDIRECT_URL = LOGIN_URL
 
-# Use custom highlighter
-HAYSTACK_CUSTOM_HIGHLIGHTER = 'voyages.extratools.TextHighlighter'
-
-# updates solr when object is saved
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-
 # import localsettings
 # This will override any previously set valaue
 try:
