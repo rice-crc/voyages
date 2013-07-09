b4a11536d964ac54bc207150493eee18c22f552c
Tomasz Jurczyk <tjurczy@emory.edu>
2013-07-09 15:59:19 -0400

    Images css files and html

--- voyages/apps/resources/urls.py
+++ voyages/apps/resources/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='resources/index.html'), name='index'),
     #handle all cases for now
-    url(r'^images$', TemplateView.as_view(template_name='under_constr.html'), name='images'),
+    url(r'^images$', TemplateView.as_view(template_name='resources/images-index.html'), name='images'),
     url(r'^names-database', TemplateView.as_view(template_name='under_constr.html'), name='origins'),
     
     url(r'^images-detail$', TemplateView.as_view(template_name='under_constr.html'), name='images-detail'),
