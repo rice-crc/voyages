a397f0933fc4189bb191ff6924f1e0c2fb067f6e
Tomasz Jurczyk <tjurczy@emory.edu>
2013-07-09 14:42:50 -0400

    Fixed ordering.

--- voyages/apps/voyage/search_indexes.py
+++ voyages/apps/voyage/search_indexes.py
@@ -9,8 +9,24 @@ class VoyageIndex(indexes.SearchIndex, indexes.Indexable):
     """
 
     voyage_in_cd_rom = indexes.BooleanField(model_attr="voyage_in_cd_rom")
+    # Groupings
     voyage_groupings_value = indexes.IntegerField()
     voyage_groupings_label = indexes.CharField()
+
+    # Captains and Owners
+    voyage_captain_name = indexes.CharField()
+    voyage_ship_owner_name = indexes.CharField()
+
+    # Voyage Ship
+    voyage_ship_ship_name = indexes.CharField
+    voyage_ship_nationality_ship_value = indexes.IntegerField()
+    voyage_ship_nationality_ship_label = indexes.CharField()
+    voyage_ship_tonnage_value = indexes.IntegerField()
+    voyage_ship_tonnage_label = indexes.CharField()
+    voyage_ship_rig_of_vessel_value = indexes.IntegerField()
+    voyage_ship_rig_of_vessel_label = indexes.CharField()
+    voyage_ship_year_of_construction = indexes.IntegerField()
+
 
     def get_model(self):
         return Voyage
@@ -24,3 +40,9 @@ class VoyageIndex(indexes.SearchIndex, indexes.Indexable):
 
     def prepare_voyage_groupings_label(self, obj):
         return obj.voyage_groupings.label
+
+    def prepare_voyage_captain_name(self, obj):
+        return obj.voyage_captain_name
+
+    def prepare_voyage_ship_owner_name(self, obj):
+        return obj.voyage_ship_owner_name
