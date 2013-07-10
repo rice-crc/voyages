4a97aa9cb67dde199f3f872f9b57d8c620bd3857
voviethoang <voviethoang@gmail.com>
2013-07-10 17:10:31 -0400

    Basic template for the GUI for search voyages

---
+++ voyages/sitemedia/scripts/voyage/voyage-search.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function() {
+
+    /* Collapsible boxes */
+    $(".box-button").click(function(ev){
+		$(this).parent().parent().toggleClass("box-collapsed box-expanded");
+	});
+
+
+    /* Fly-out menus */
+    $(".menu-popup-item-main, .menu-popup-item-main-last").hover(function() {
+		$(this).children(".menu-popup-submenu-frame").removeClass("hidden");
+		return true;
+	}, function() {
+        $(this).children(".menu-popup-submenu-frame").addClass("hidden");
+	});
+
+
+});
