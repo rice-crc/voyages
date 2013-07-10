1f3d79fd9d3f2189a6b52681fe75969d7b08f516
Tomasz Jurczyk <tjurczy@emory.edu>
2013-07-10 17:15:38 -0400

    Model and admin for Image module

--- voyages/apps/resources/models.py
+++ voyages/apps/resources/models.py
@@ -1,21 +1,26 @@
 from django.db import models
+from os.path import basename
 from voyages.apps.voyage.models import Voyage
 
 class Image(models.Model):
-    file_name = models.CharField(max_length=256, null=True)
+    """
+    Model to store information about image.
+    """
+
+    file = models.FileField(upload_to='images')
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=2000, null=True)
-    width = models.IntegerField(null=True)
-    height = models.IntegerField(null=True)
-    mime_type = models.CharField(max_length=100, null=True)
-    creator = models.CharField(max_length=200, null=True)
-    language = models.CharField(max_length=2, null=True)
-    size = models.IntegerField(null=True)
-    source = models.CharField(max_length=500, null=True)
-    comments = models.CharField(max_length=2000, null=True)
-    other_references = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=2000, null=True, blank=True)
+    width = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
+    mime_type = models.CharField(max_length=100, null=True, blank=True)
+    creator = models.CharField(max_length=200, null=True, blank=True)
+    language = models.CharField(max_length=2, null=True, blank=True)
+    size = models.IntegerField(null=True, blank=True)
+    source = models.CharField(max_length=500, null=True, blank=True)
+    comments = models.CharField(max_length=2000, null=True, blank=True)
+    other_references = models.CharField(max_length=500, null=True, blank=True)
     emory = models.BooleanField()
-    emory_location = models.CharField(max_length=500, null=True)
+    emory_location = models.CharField(max_length=500, null=True, blank=True)
     authorization_status = models.IntegerField()
     image_status = models.IntegerField()
     ready_to_go = models.BooleanField()
@@ -26,7 +31,32 @@ class Image(models.Model):
 
     # Category
     category = models.ForeignKey('ImageCategory', verbose_name="Image category")
-    voyage = models.ForeignKey(Voyage)
+    # FIXME: 'null=True, blank=True' will be removed (for tests)
+    voyage = models.ForeignKey(Voyage, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+
+    def get_file_name(self):
+        """
+        Returns file name of each file
+        """
+        return basename(self.file.name)
+
+    def __unicode__(self):
+        return str(self.id) + ", " + self.get_file_name()
 
 class ImageCategory(models.Model):
+    """
+    Model stores categories for images.
+    """
+
     name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Image Category"
+        verbose_name_plural = "Image Categories"
+
+    def __unicode__(self):
+        return str(self.id) + ", " + self.name
