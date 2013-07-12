74a5a21ba9420d1d2371f501cfad3d34ed401e25
voviethoang <voviethoang@gmail.com>
2013-07-12 16:05:01 -0400

    Updated images.json (removed double quotation marks)

--- voyages/apps/resources/admin.py
+++ voyages/apps/resources/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import *
+from .forms import *
 
 class ImageAdmin(admin.ModelAdmin):
     list_filter = ['category']
     list_display = ['id', 'file', 'title']
+    form = ImageForm
 
     class Meta:
         model = Image
