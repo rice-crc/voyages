7a04587e380927b33e91e78246d0e385331edbef
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-07-17 17:23:22 -0400

    Added back to category

--- voyages/apps/resources/templatetags/resources_extras.py
+++ voyages/apps/resources/templatetags/resources_extras.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 @register.filter
 @stringfilter
-
 def get_year_value(value):
     """ use the django template loader and response object to spit
     out rendered content
@@ -15,3 +14,8 @@ def get_year_value(value):
     except:
         ret = ""
     return ret
+
+
+@register.filter
+def modulo(num, val):
+    return num % val
