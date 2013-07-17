ed471f08054a8d34b29d55c0e25a5439efafea16
Tomasz Jurczyk <tjurczy@emory.edu>
2013-07-17 11:42:56 -0400

    Added template tags for resources

---
+++ voyages/apps/resources/templatetags/resources_extras.py
@@ -0,0 +1,17 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+
+def get_year_value(value):
+    """ use the django template loader and response object to spit
+    out rendered content
+    """
+    try:
+        ret = value.split(",")[2]
+    except:
+        ret = ""
+    return ret
