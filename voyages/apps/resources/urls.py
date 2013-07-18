d5432f2f750dd5f6f6b28434502779329b0a77ac
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-07-18 15:55:51 -0400

    Finished images

--- voyages/apps/resources/urls.py
+++ voyages/apps/resources/urls.py
@@ -1,15 +1,25 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-# To be used later
-# from voyages.apps.resources.views import *
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='resources/index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='resources/index.html'),
+        name='index'),
     #handle all cases for now
-    url(r'^images/$', 'voyages.apps.resources.views.get_all_images', name='images'),
-    url(r'^images/category/(?P<category>\w+)$', 'voyages.apps.resources.views.get_images_category', name='images-category'),
-    url(r'^images/category/(?P<category>\w+)/(?P<page>\d{1,3})$', 'voyages.apps.resources.views.get_images_category_detail', name='images-category-detail'),
-    url(r'^names-database', TemplateView.as_view(template_name='under_constr.html'), name='origins'),
-    url(r'^[\w\.\-]+\$', TemplateView.as_view(template_name='under_constr.html')),
+    url(r'^images/$', 'voyages.apps.resources.views.get_all_images',
+        name='images'),
+    url(r'^images/category/(?P<category>\w+)$',
+        'voyages.apps.resources.views.get_images_category',
+        name='images-category'),
+    url(r'^images/category/(?P<category>\w+)/(?P<page>\d{1,3})$',
+        'voyages.apps.resources.views.get_images_category_detail',
+        name='images-category-detail'),
+    url(r'^images/category/(?P<category>\w+)/(?P<page>\d{1,3})/detail$',
+        'voyages.apps.resources.views.get_image_detail',
+        name='image-detail.html'),
+    url(r'^names-database',
+        TemplateView.as_view(template_name='under_constr.html'),
+        name='origins'),
+    url(r'^[\w\.\-]+\$',
+        TemplateView.as_view(template_name='under_constr.html')),
 )
--- voyages/apps/resources/views.py
+++ voyages/apps/resources/views.py
@@ -1,8 +1,7 @@
-from django.template import TemplateDoesNotExist, Context, loader, RequestContext
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.utils.datastructures import SortedDict
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from voyages.apps.voyage.models import *
+from django.core.paginator import Paginator
 from .models import *
 
 
@@ -10,7 +9,9 @@ from .models import *
 def get_all_images(request):
     """
     View to get demo images (4 per group).
+    :param request: Request to serve
     """
+
     images = SortedDict()
 
     for i in ImageCategory.objects.all().order_by("-value"):
@@ -30,12 +31,17 @@ def get_all_images(request):
 def get_images_category(request, category):
     """
     View to show images by group.
+
+    :param request: Request to serve
+    :param category: Get images from this category
     """
 
     images = SortedDict()
 
+    # Pack all images from category with needed data.
     for i in Image.objects.filter(category__label=category, ready_to_go=True).order_by('date', 'image_id'):
         images[i.image_id] = SortedDict({'file': i.file, 'year': i.date, 'title': i.title})
+
     return render_to_response('resources/images-category.html',
                               {'images': images, 'category': category},
                               context_instance=RequestContext(request))
@@ -44,12 +50,32 @@ def get_images_category(request, category):
 def get_images_category_detail(request, category, page):
     """
     View to show images by group in detail.
+    :param request: Request to serve
+    :param category: Get images from this category
+    :param page: Number of page to serve
     """
+
     manu = Image.objects.filter(category__label=category, ready_to_go=True).order_by('date', 'image_id')
 
+    # Set paginator on proper page.
     paginator = Paginator(manu, 1)
     pagins = paginator.page(page)
 
     return render_to_response('resources/image-category-detail.html',
                               {'images': pagins, 'category': category},
+                              context_instance=RequestContext(request))
+
+
+def get_image_detail(request, category, page):
+    """
+    View to show images in detail.
+    :param request: Request to serve
+    :param category: Get images from this category
+    :param page: Number of page to serve
+    """
+
+    image = Image.objects.filter(category__label=category, ready_to_go=True).order_by('date', 'image_id')[int(page)]
+
+    return render_to_response('resources/image-detail.html',
+                              {'image': image},
                               context_instance=RequestContext(request))
