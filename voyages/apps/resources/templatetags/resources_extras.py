f013ae902bf851220acfa27f9f4d8f9e950bd0d5
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-07-18 15:55:27 -0400

    Added filter to languages in images-detail

--- voyages/apps/resources/templatetags/resources_extras.py
+++ voyages/apps/resources/templatetags/resources_extras.py
@@ -19,3 +19,20 @@ def get_year_value(value):
 @register.filter
 def modulo(num, val):
     return num % val
+
+@register.filter
+def decode_language(code):
+    if code == "en":
+        return "English"
+    if code == "fr":
+        return "French"
+    if code == "de":
+        return "German"
+    if code == "pt":
+        return "Portuguese"
+    if code == "nl":
+        return "Dutch"
+    if code == "la":
+        return "Latin"
+    if code == "es":
+        return "Spanish"
