600fc6c30259a4ff7cb161b656667b6141f3cc9e
Alex Thomas <athom09@emory.edu>
2013-07-23 21:24:57 -0400

    Update fabfile.py
    added --noinput to test command call

--- fabfile.py
+++ fabfile.py
@@ -30,7 +30,7 @@ def test():
 
     # We are specifying apps to test because of problem with autocomplete_light.
     # There is missing some sub app to test and it causes fail of all tests.
-    local('python manage.py test about assessment common contribute education help resources voyage')
+    local('python manage.py test --noinput about assessment common contribute education help resources voyage')
 
 
 def doc():
