fcc4bf3d52a8a558df45f0748e5c04c7e3e91374
voviethoang <voviethoang@gmail.com>
2013-07-29 11:47:37 -0400

    Updated search_indexes.py (fixed correct types of fields) and updated solr/schema.xml

--- voyages/apps/voyage/search_indexes.py
+++ voyages/apps/voyage/search_indexes.py
@@ -9,42 +9,42 @@ class VoyageIndex(indexes.SearchIndex, indexes.Indexable):
     """
     text = indexes.CharField(document=True, use_template=True)
 
-    var_voyage_id = indexes.IntegerField(model_attr='voyage_id')
-    var_voyage_in_cd_rom = indexes.BooleanField(model_attr="voyage_in_cd_rom")
-    var_ship_name = indexes.CharField()
-    var_nationality = indexes.IntegerField()
-    var_imputed_nationality = indexes.IntegerField()
-    var_year_of_construction = indexes.IntegerField()
-    var_registered_place = indexes.IntegerField()
-    var_registered_year = indexes.IntegerField()
-    var_rig_of_vessel = indexes.IntegerField()
-    var_tonnage = indexes.IntegerField()
-    var_tonnage_mod = indexes.IntegerField()
-    var_guns_mounted = indexes.IntegerField()
-    var_owner = indexes.CharField()
+    var_voyage_id = indexes.IntegerField(model_attr='voyage_id', null=True)
+    var_voyage_in_cd_rom = indexes.BooleanField(model_attr="voyage_in_cd_rom", null=True)
+    var_ship_name = indexes.CharField(null=True)
+    var_nationality = indexes.IntegerField(null=True)
+    var_imputed_nationality = indexes.IntegerField(null=True)
+    var_year_of_construction = indexes.IntegerField(null=True)
+    var_registered_place = indexes.IntegerField(null=True)
+    var_registered_year = indexes.IntegerField(null=True)
+    var_rig_of_vessel = indexes.IntegerField(null=True)
+    var_tonnage = indexes.IntegerField(null=True)
+    var_tonnage_mod = indexes.IntegerField(null=True)
+    var_guns_mounted = indexes.IntegerField(null=True)
+    var_owner = indexes.CharField(null=True)
 
     # Captains and Owners
-    var_captain = indexes.CharField()
-    var_owner = indexes.CharField()
-    voyage_ship_owner_name = indexes.CharField()
+    var_captain = indexes.CharField(null=True)
+    var_owner = indexes.CharField(null=True)
+    voyage_ship_owner_name = indexes.CharField(null=True)
 
     # Voyage Ship
-    voyage_ship_nationality_ship_value = indexes.IntegerField()
-    voyage_ship_nationality_ship_label = indexes.CharField()
-    voyage_ship_tonnage_value = indexes.IntegerField()
-    voyage_ship_tonnage_label = indexes.CharField()
-    voyage_ship_rig_of_vessel_value = indexes.IntegerField()
-    voyage_ship_rig_of_vessel_label = indexes.CharField()
-    voyage_ship_year_of_construction = indexes.IntegerField()
+    voyage_ship_nationality_ship_value = indexes.IntegerField(null=True)
+    voyage_ship_nationality_ship_label = indexes.CharField(null=True)
+    voyage_ship_tonnage_value = indexes.IntegerField(null=True)
+    voyage_ship_tonnage_label = indexes.CharField(null=True)
+    voyage_ship_rig_of_vessel_value = indexes.IntegerField(null=True)
+    voyage_ship_rig_of_vessel_label = indexes.CharField(null=True)
+    voyage_ship_year_of_construction = indexes.IntegerField(null=True)
 
-    var_outcome_voyage = indexes.IntegerField()
-    var_outcome_slaves = indexes.IntegerField()
-    var_outcome_owner = indexes.IntegerField()
-    var_resistance = indexes.IntegerField()
-    var_outcome_ship_captured = indexes.IntegerField()
+    var_outcome_voyage = indexes.CharField(null=True)
+    var_outcome_slaves = indexes.CharField(null=True)
+    var_outcome_owner = indexes.CharField(null=True)
+    var_resistance = indexes.CharField(null=True)
+    var_outcome_ship_captured = indexes.CharField(null=True)
 
     # Sources
-    var_sources = indexes.MultiValueField()
+    var_sources = indexes.MultiValueField(null=True)
 
     def get_model(self):
         return Voyage
@@ -54,97 +54,200 @@ class VoyageIndex(indexes.SearchIndex, indexes.Indexable):
         return self.get_model().objects.all()
 
     def prepare_var_ship_name(self, obj):
-        return obj.voyage_ship.ship_name
+        try:
+            return obj.voyage_ship.ship_name
+        except AttributeError:
+            return None
 
     def prepare_var_nationality(self, obj):
-        return obj.voyage_ship.nationality_ship.pk
+        try:
+            return obj.voyage_ship.nationality_ship.label
+        except AttributeError:
+            return None
 
     def prepare_var_imputed_nationality(self, obj):
-        return obj.voyage_ship.nationality_ship.pk
+        try:
+            return obj.voyage_ship.nationality_ship.label
+        except AttributeError:
+            return None
 
     def prepare_var_vessel_construction_place(self, obj):
-        return obj.voyage_ship.vessel_construction_place.pk
+        try:
+            return obj.voyage_ship.vessel_construction_place.place
+        except AttributeError:
+            return None
 
     def prepare_var_year_of_construction(self, obj):
-        return obj.voyage_ship.year_of_construction
+        try:
+            return obj.voyage_ship.year_of_construction
+        except AttributeError:
+            return None
 
     def prepare_var_registered_place(self, obj):
-        return obj.voyage_ship.registered_place.pk
+        try:
+            return obj.voyage_ship.registered_place.place
+        except AttributeError:
+            return None
 
     def prepare_var_registered_year(self, obj):
-        return obj.voyage_ship.registered_year
+        try:
+            return obj.voyage_ship.registered_year
+        except AttributeError:
+            return None
 
     def prepare_var_var_rig_of_vessel(self, obj):
-        return obj.voyage_ship.rig_of_vessel.pk
+        try:
+            return obj.voyage_ship.rig_of_vessel.label
+        except AttributeError:
+            return None
 
     def prepare_var_tonnage(self, obj):
-        return obj.voyage_ship.tonnage
+        try:
+            return obj.voyage_ship.tonnage
+        except AttributeError:
+            return None
 
     def prepare_var_tonnage_mod(self, obj):
-        return obj.voyage_ship.tonnage_mod
+        try:
+            return obj.voyage_ship.tonnage_mod
+        except AttributeError:
+            return None
 
     def prepare_var_guns_mounted(self, obj):
-        return obj.voyage_ship.guns_mounted
+        try:
+            return obj.voyage_ship.guns_mounted
+        except AttributeError:
+            return None
 
     def prepare_var_owner(self, obj):
-        return [connection.owner.name for connection in VoyageShipOwnerConnection.objects.filter(voyage=obj)]
+        try:
+            return [connection.owner.name for connection in VoyageShipOwnerConnection.objects.filter(voyage=obj)]
+        except AttributeError:
+            return None
 
     def prepare_var_outcome_voyage(self, obj):
-        return VoyageOutcome.objects.filter(voyage=obj)[0].particular_outcome.pk
+        try:
+            return VoyageOutcome.objects.filter(voyage=obj)[0].particular_outcome.label
+        except AttributeError:
+            return None
+        except IndexError:
+            return None
 
     def prepare_var_outcome_slaves(self, obj):
-        return VoyageOutcome.objects.filter(voyage=obj)[0].outcome_slaves.pk
+        try:
+            return VoyageOutcome.objects.filter(voyage=obj)[0].outcome_slaves.label
+        except AttributeError:
+            return None
+        except IndexError:
+            return None
 
     def prepare_var_outcome_owner(self, obj):
-        return VoyageOutcome.objects.filter(voyage=obj)[0].outcome_owner.pk
+        try:
+            return VoyageOutcome.objects.filter(voyage=obj)[0].outcome_owner.label
+        except AttributeError:
+            return None
+        except IndexError:
+            return None
 
     def prepare_var_resistance(self, obj):
-        return VoyageOutcome.objects.filter(voyage=obj)[0].resistance.pk
+        try:
+            return VoyageOutcome.objects.filter(voyage=obj)[0].resistance.label
+        except AttributeError:
+            return None
+        except IndexError:
+            return None
 
     def prepare_var_outcome_ship_captured(self, obj):
-        return VoyageOutcome.objects.filter(voyage=obj)[0].vessel_captured_outcome.label
+        try:
+            return VoyageOutcome.objects.filter(voyage=obj)[0].vessel_captured_outcome.label
+        except AttributeError:
+            return None
+        except IndexError:
+            return None
 
     def prepare_var_port_of_departure(self, obj):
-        return obj.voyage_itinerary.port_of_departure
+        try:
+            return obj.voyage_itinerary.port_of_departure.place
+        except AttributeError:
+            return None
 
     def prepare_var_first_place_slave_purchase(self, obj):
-        return obj.voyage_itinerary.first_place_slave_purchase.place
+        try:
+            return obj.voyage_itinerary.first_place_slave_purchase.place
+        except AttributeError:
+            return None
 
     def prepare_var_second_place_slave_purchase(self, obj):
-        return obj.voyage_itinerary.second_place_slave_purchase.place
+        try:
+            return obj.voyage_itinerary.second_place_slave_purchase.place
+        except AttributeError:
+            return None
 
     def prepare_var_third_place_slave_purchase(self, obj):
-        return obj.voyage_itinerary.third_place_slave_purchase.place
+        try:
+            return obj.voyage_itinerary.third_place_slave_purchase.place
+        except AttributeError:
+            return None
 
     def prepare_var_principal_place_of_slave_purchase(self, obj):
-        return obj.voyage_itinerary.principal_place_of_slave_purchase.place
+        try:
+            return obj.voyage_itinerary.principal_place_of_slave_purchase.place
+        except AttributeError:
+            return None
 
     def prepare_var_port_of_call_before_atl_crossing(self, obj):
-        return obj.voyage_itinerary.port_of_call_before_atl_crossing.place
+        try:
+            return obj.voyage_itinerary.port_of_call_before_atl_crossing.place
+        except AttributeError:
+            return None
 
     def prepare_var_first_landing_place(self, obj):
-        return obj.voyage_itinerary.first_landing_place.place
+        try:
+            return obj.voyage_itinerary.first_landing_place.place
+        except AttributeError:
+            return None
 
     def prepare_var_second_landing_place(self, obj):
-        return obj.voyage_itinerary.second_landing_place.place
+        try:
+            return obj.voyage_itinerary.second_landing_place.place
+        except AttributeError:
+            return None
 
     def prepare_var_third_landing_place(self, obj):
-        return obj.voyage_itinerary.third_landing_place.place
+        try:
+            return obj.voyage_itinerary.third_landing_place.place
+        except AttributeError:
+            return None
 
     def prepare_var_principal_port_of_slave_dis(self, obj):
-        return obj.voyage_itinerary.principal_port_of_slave_dis.place
+        try:
+            return obj.voyage_itinerary.principal_port_of_slave_dis.place
+        except AttributeError:
+            return None
 
     def prepare_var_place_voyage_ended(self, obj):
-        return obj.voyage_itinerary.var_place_voyage_ended.place
+        try:
+            return obj.voyage_itinerary.var_place_voyage_ended.place
+        except AttributeError:
+            return None
 
     def prepare_var_imp_port_voyage_begin(self, obj):
-        return obj.voyage_itinerary.imp_port_voyage_begin.place
+        try:
+            return obj.voyage_itinerary.imp_port_voyage_begin.place
+        except AttributeError:
+            return None
 
     def prepare_var_imp_principal_place_of_slave_purchase(self, obj):
-        return obj.voyage_itinerary.imp_principal_place_of_slave_purchase.place
+        try:
+            return obj.voyage_itinerary.imp_principal_place_of_slave_purchase.place
+        except AttributeError:
+            return None
 
     def prepare_var_imp_principal_port_slave_dis(self, obj):
-        return obj.voyage_itinerary.imp_principal_port_slave_dis.place
+        try:
+            return obj.voyage_itinerary.imp_principal_port_slave_dis.place
+        except AttributeError:
+            return None
 
     def prepare_var_captain(self, obj):
         return [connection.captain.name for connection in VoyageCaptainConnection.objects.filter(voyage=obj)]
@@ -189,5 +292,4 @@ class VoyageIndex(indexes.SearchIndex, indexes.Indexable):
         return obj.voyage_slaves_numbers.imp_total_slaves_disembarked
 
     def prepare_var_sources(self, obj):
-        return [connection.captain.name for connection in VoyageSourcesConnection.objects.filter(voyage=obj)]
-
+        return obj.voyage_sources
