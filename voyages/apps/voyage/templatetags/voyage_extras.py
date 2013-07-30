5687fd375512a30b6f08047506abe2a2f41bc26b
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-07-30 16:02:02 -0400

    Table for voyages search

--- voyages/apps/voyage/templatetags/voyage_extras.py
+++ voyages/apps/voyage/templatetags/voyage_extras.py
@@ -14,3 +14,16 @@ def parse_blocks(value):
     t = Template(value)
     c = Context({ 'MEDIA_URL': settings.MEDIA_URL })
     return t.render(c)
+
+
+@register.filter
+@stringfilter
+def get_year_value(value):
+    """ use the django template loader and response object to spit
+    out rendered content
+    """
+    try:
+        ret = value.split(",")[2]
+    except:
+        ret = ""
+    return ret
