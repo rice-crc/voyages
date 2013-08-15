5762d2879ab3e77396985284b0d4fa82c86b659c
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-08-15 18:08:03 -0400

    Documentary sources sketch

--- voyages/apps/voyage/urls.py
+++ voyages/apps/voyage/urls.py
@@ -4,16 +4,16 @@ from django.views.generic import TemplateView, RedirectView
 urlpatterns = patterns('',
     url(r'^c(?P<chapternum>\d{2})_s(?P<sectionnum>\d{2})_p(?P<pagenum>\d{2})$',
         'voyages.apps.voyage.views.get_page', name='get-page'),
+     url(r'^source_(?P<category>\w+)$',
+        'voyages.apps.voyage.views.sources_list', name='sources-list')
 )
 
 urlpatterns += patterns('',
     url(r'^$', TemplateView.as_view(template_name='voyage/index.html'), name='index'),
     url(r'^understanding-db$', TemplateView.as_view(template_name='voyage/c01_base.html'), name='guide'),
-    
+
     url(r'^c01_s01_cover$', TemplateView.as_view(template_name='voyage/c01_s01_cover.html'), name='voyage-guide-intro'),
     url(r'^c01_s03_cover$', 'voyages.apps.voyage.views.variable_list', name='variables'),
-    url(r'^sources/(?P<category>\w*)$', 'voyages.apps.voyage.views.sources_list', name='sources'),
-    url(r'^sources$', 'voyages.apps.voyage.views.sources_list', name='sources'),
     url(r'^search', 'voyages.apps.voyage.views.search', name='search'),
 
     url(r'^contribute$', RedirectView.as_view(url='/contribute'), name='submission-login'),
