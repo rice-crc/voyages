fbd51066fd3a66c14c3f3f81f0dc695d80bca255
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-08-19 09:41:36 -0400

    Printing documentary sources (unsorted)

--- voyages/apps/voyage/urls.py
+++ voyages/apps/voyage/urls.py
@@ -4,7 +4,9 @@ from django.views.generic import TemplateView, RedirectView
 urlpatterns = patterns('',
     url(r'^c(?P<chapternum>\d{2})_s(?P<sectionnum>\d{2})_p(?P<pagenum>\d{2})$',
         'voyages.apps.voyage.views.get_page', name='get-page'),
-     url(r'^source_(?P<category>\w+)$',
+     url(r'^source/(?P<category>\w+)[/]?(?P<sort>\w*)$',
+        'voyages.apps.voyage.views.sources_list', name='sources-list-sort'),
+     url(r'^source/(?P<category>\w+)$',
         'voyages.apps.voyage.views.sources_list', name='sources-list')
 )
 
