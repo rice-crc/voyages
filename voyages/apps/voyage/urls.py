ad824da03e756247c0671547ca706155a2cd5c8f
Tomasz Jurczyk <dzordzyk@gmail.com>
2013-09-04 12:27:48 -0400

    hotfix for understanding db and sources

--- voyages/apps/voyage/urls.py
+++ voyages/apps/voyage/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = patterns('',
-    url(r'^c(?P<chapternum>\d{2})_s(?P<sectionnum>\d{2})_p(?P<pagenum>\d{2})$',
+    url(r'^c(?P<chapternum>\w{2})_s(?P<sectionnum>\w{2})_p(?P<pagenum>\w{2})$',
         'voyages.apps.voyage.views.get_page', name='get-page'),
     url(r'^source/(?P<category>\w+)/(?P<sort>\w+)$',
         'voyages.apps.voyage.views.sources_list', name='sources-list-sort'),
