7ee3681e276d028fc85ab396b61bb1cce31874f9
Alex Thomas <athom09@emory.edu>
2014-02-25 13:21:40 -0500

    added ability for users to login via username or email

--- voyages/apps/common/tests.py
+++ voyages/apps/common/tests.py
@@ -1,1 +1,21 @@
 from django.test import TestCase
+class UserOrEmailTest(TestCase):
+
+    fixtures = ['users.json']
+
+    def test_login(self):
+        # using username with bad password
+        result = self.client.login(username='testuser', password='xxxxxx')
+        self.assertFalse(result)
+
+        # using username with good password
+        result = self.client.login(username='testuser', password='testuser')
+        self.assertTrue(result)
+
+        # using email with bad password
+        result = self.client.login(username='test@user.com', password='xxxxxx')
+        self.assertFalse(result)
+
+        # using email with good password
+        result = self.client.login(username='test@user.com', password='testuser')
+        self.assertTrue(result)
