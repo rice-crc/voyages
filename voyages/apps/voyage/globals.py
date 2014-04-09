c978429906d96ee4f6b64f8cac6027fa5b8aa572
Blake Rainwater <rainwaterblake@gmail.com>
2014-04-09 14:36:01 -0400

    Added month list without functionality

--- voyages/apps/voyage/globals.py
+++ voyages/apps/voyage/globals.py
@@ -11,8 +11,8 @@ list_imputed_nationality_values = ['Spain / Uruguay', 'Portugal / Brazil', 'Grea
                                    'Netherlands', 'U.S.A.', 'France', 'Denmark / Baltic',
                                    'Other (specify in note)']
 
-list_months = [('Jan', '01'), ('Feb', '02'), ('Mar', '03'), ('Apr', '04'), ('May', '05'), ('Jun', '06'),
-               ('Jul', '07'), ('Aug', '08'), ('Sep', '09'), ('Oct', '10'), ('Nov', '11'), ('Dec', '12')]
+list_months = [('01', 'Jan'), ('02', 'Feb'), ('03', 'Mar'), ('04', 'Apr'), ('05', 'May'), ('06', 'Jun'),
+               ('07', 'Jul'), ('08', 'Aug'), ('09', 'Sep'), ('10', 'Oct'), ('11', 'Nov'), ('12', 'Dec')]
 
 
 def structure_places(place_list):
