5d54bbe198c6e40c70451873af5c3b02425b0eff
Blake Rainwater <rainwaterblake@gmail.com>
2014-05-23 18:01:15 -0400

    Fix off by one error that occurred due to a difference in openpyxl 1.9

--- voyages/apps/voyage/views.py
+++ voyages/apps/voyage/views.py
@@ -904,7 +904,7 @@ def search(request):
                 ws = wb.active
                 for idx, i in enumerate(xls_table):
                     for idy, j in enumerate(i):
-                        ws.cell(row=idx,column=idy).value = unicode(j).encode('utf-8')
+                        ws.cell(row=idx+1,column=idy+1).value = unicode(j).encode('utf-8')
                 wb.save(response)
                 return response
         elif submitVal == 'tab_graphs':
