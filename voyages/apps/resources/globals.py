f9054a66b25439425efba7540955208233148db7
Tomasz Jurczyk <dzordzyk@gmail.com>
2014-07-13 14:34:40 -0400

    Added search for Africans, Origin and Embarkation

---
+++ voyages/apps/resources/globals.py
@@ -0,0 +1,8 @@
+from .models import Country, AfricanName
+
+names_search_strict_text = ['slave_name', 'slave_ship_name', 'slave_voyage_number', 'slave_date_arrived__gte',
+                            'slave_date_arrived__lte', 'slave_age__gte', 'slave_age__lte', 'slave_height__gte',
+                            'slave_height__lte']
+
+names_search_checkboxes = ['search_slave_sex_boys', 'search_slave_sex_girls', 'search_slave_sex_men',
+                           'search_slave_sex_women', 'search_slave_sex_males', 'search_slave_sex_females']
