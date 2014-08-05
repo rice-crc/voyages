baaa87ddfd7f40cd3ffdf34cf48d3474f52698ba
Tomasz Jurczyk <dzordzyk@gmail.com>
2014-08-05 15:38:28 -0400

    Added models for areas and regions

--- voyages/apps/assessment/models.py
+++ voyages/apps/assessment/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class ExportArea(models.Model):
+    """
+    Class represents Export area entity.
+    """
+
+    area_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200, verbose_name="Export area name")
+    order_num = models.IntegerField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    show_at_zoom = models.IntegerField()
+    show_on_map = models.BooleanField()
+
+
+class ExportRegion(models.Model):
+    """
+    Class represents Export region entity.
+    """
+    region_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200, verbose_name="Export region name")
+    order_num = models.IntegerField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    show_at_zoom = models.IntegerField()
+    show_on_map = models.BooleanField()
+
+
+class ImportArea(models.Model):
+    """
+    Class represents Import area entity.
+    """
+    area_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200, verbose_name="Import area name")
+    order_num = models.IntegerField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    show_at_zoom = models.IntegerField()
+    show_on_map = models.BooleanField()
+
+
+class ImportRegion(models.Model):
+    """
+    Class represents Import region entity.
+    """
+    region_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200, verbose_name="Import region name")
+    order_num = models.IntegerField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    show_at_zoom = models.IntegerField()
+    show_on_map = models.BooleanField()
