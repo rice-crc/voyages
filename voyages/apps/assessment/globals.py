08278426e3d4b8a1efb41f86eeda65b00af3c4e8
Tomasz Jurczyk <dzordzyk@gmail.com>
2014-08-28 14:45:17 -0400

    Partially working initial view

--- voyages/apps/assessment/globals.py
+++ voyages/apps/assessment/globals.py
@@ -1,11 +1,77 @@
-table_rows = ["Flag", "Embarkation regions", "Broad disembarkation regions",
-              "Specific disembarkation regions", "Individual years", "5-year periods",
-              "10-year periods", "25-year periods", "50-year periods", "100-year periods"]
+from haystack.query import SearchQuerySet
+from .models import *
 
-table_columns = ["Flag", "Embarkation regions", "Broad disembarkation regions",
-                 "Specific disembarkation regions"]
 
-table_cells = ["Embarked/Disembarked", "Only embarked", "Only disembarked"]
+def get_flags():
+    a = SearchQuerySet().models(Nation)
+    nations = [(k.name, 1) for k in a]
+    return [nations, ]
+
+
+def get_broad_regions(area):
+    areas_from_query = SearchQuerySet().models(area)
+    areas = [(k.name, 1) for k in areas_from_query]
+    print "areas = " + str(areas)
+
+    return [areas, ]
+
+
+def get_regions(region, area, mode="export"):
+    return_areas = []
+    return_regions = []
+    areas = SearchQuerySet().models(area)
+
+    # For each area, retrieve list of regions and add to the list with an appropriate length
+    for local_area in areas:
+
+        # Retrieve regions
+        if mode == "export":
+            local_regions = SearchQuerySet().models(region).filter(export_area__exact=local_area.name)
+        else:
+            local_regions = SearchQuerySet().models(region).filter(import_area__exact=local_area.name)
+
+        if len(local_regions) == 0:
+            continue
+        # Add the area
+        return_areas.append((local_area.name, len(local_regions)))
+
+        # Add regions of this area to the list
+        return_regions.extend((k.name, 1) for k in local_regions)
+
+    return [return_areas, return_regions]
+
+
+def get_embarkation_regions():
+    areas = SearchQuerySet().models(ExportRegion)
+
+    return_areas = [(k.name, 1) for k in areas]
+
+    return [return_areas, ]
+
+
+table_rows = [("Flag", "nation", get_flags()),
+              ("Embarkation regions", "embarkation_region", get_embarkation_regions()),
+              ("Broad disembarkation regions", "broad_disembarkation_region", get_broad_regions(ImportArea)),
+              ("Specific disembarkation regions", "disembarkation_region", get_regions(ImportRegion, ImportArea, "import")),
+              ("Individual years", ),
+              ("5-year periods", ),
+              ("10-year periods", ),
+              ("25-year periods", ),
+              ("50-year periods", ),
+              ("100-year periods", )]
+
+table_columns = [("Flag", "nation", get_flags()),
+                 ("Embarkation regions", "embarkation_region", get_embarkation_regions()),
+                 ("Broad disembarkation regions", "broad_disembarkation_region", get_broad_regions(ImportArea)),
+                 ("Specific disembarkation regions", "disembarkation_region",
+                  get_regions(ImportRegion, ImportArea, "import"))]
+
+table_cells = [("Embarked/Disembarked", ),
+               ("Only embarked", ),
+               ("Only disembarked", )]
+
+
+
 
 # These two have to be replaced with context processor call
 # on database/solr
--- voyages/apps/assessment/views.py
+++ voyages/apps/assessment/views.py
@@ -54,10 +54,97 @@ def get_estimates(request):
         year_form = EstimateYearForm(initial={'frame_from_year': globals.first_year,
                                               'frame_to_year': globals.last_year})
 
+    results = SearchQuerySet().models(Estimate)
+    column_query_set = globals.table_columns[0][2]
+    column_variable_name = globals.table_columns[0][1]
+    column_sums = []
+
+    print "column_query_set = " + str(column_query_set)
+
+    row_query_set = globals.table_rows[2][2]
+    row_variable_name = globals.table_rows[2][1]
+    rows_sums = []
+
+    print "row_query_set = " + str(row_query_set)
+
+    columns_before_titles = 1
+    row_list = []
+
+    # Perform query
+    # Iterate through columns and collect sums. Omit if necessary (omit checked).
+    for col_query_item in column_query_set[-1]:
+        print "query dict = " + column_variable_name + "__exact"
+        print "query value = " + str(col_query_item[0])
+        query_dict = {column_variable_name + "__exact": col_query_item[0]}
+        a = results.filter(**query_dict)
+        print "a = " + str(a)
+        sum_values = sum([int(b.embarked_slaves) for b in a])
+        column_sums.append(sum_values)
+
+    if row_variable_name == "disembarkation_region":
+        area_ranges = create_area_ranges(row_query_set[0])
+        columns_before_titles = 2
+
+    # Iterate through rows
+    row_total = 0
+    row_counter = 1
+    for row_query_item in row_query_set[-1]:
+        if row_query_item[1] != 1:
+            print "CONTINUE"
+            continue
+        row_query_dict = {row_variable_name + "__exact": row_query_item[0] }
+
+        row_list_item = []
+
+        a = results.filter(**row_query_dict)
+        print "for row = " + str(row_query_item[0])
+
+        row_values_list = []
+        if row_variable_name == "disembarkation_region" and row_counter in area_ranges:
+            index = area_ranges.index(row_counter)
+            row_list_item.append([row_query_set[0][index], row_query_item, ])
+        else:
+            row_list_item.append([row_query_item, ])
+
+        row_counter += 1
+        for col_query_item in column_query_set[-1]:
+            if col_query_item[0] == "Totals":
+                continue
+            column_query_dict = {column_variable_name + "__exact": col_query_item[0]}
+            b = a.filter(**column_query_dict)
+            row_values_list.append(sum([int(c.embarked_slaves) for c in b]))
+            #print "sum for " + str(col_query_item[0]) + " = " + str(sum([int(c.embarked_slaves) for c in b]))
+
+        row_list_item.append(row_values_list)
+        row_sum = sum(row_values_list)
+        row_list_item.append(row_sum)
+        row_total += row_sum
+        row_list.append(row_list_item)
+
+    print "at the end, column query set = " + str(column_query_set)
+    column_sums.append(row_total)
+    print "row_list = " + str(row_list)
+    print "sum = " + str(column_sums)
+
     return render(request, 'assessment/estimates.html',
         {'first_year': first_year,
          'last_year': last_year,
          'year_form': year_form,
          'export_regions': collections.OrderedDict(sorted(export_regions.items(), key=lambda x: x[0].name)),
          'import_regions': collections.OrderedDict(sorted(import_regions.items(), key=lambda x: x[0].name)),
+         'col_labels': column_query_set,
+         'row_list': row_list,
+         'col_sums': column_sums,
+         'columns_before_titles': columns_before_titles,
          'table_form': form})
+
+
+def create_area_ranges(areas):
+    values = []
+    values.append(areas[0][1])
+    for i, area in enumerate(areas):
+        if i > 0:
+            values.append(sum([k[1] for k in areas[0:i]]) + 1)
+
+    print "values = " + str(values)
+    return values
