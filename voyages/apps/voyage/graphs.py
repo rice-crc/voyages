9ddef68290f4a9c259ba5c763f8c488c044d99e2
Domingos <domingos.junior@gmail.com>
2016-02-12 16:54:19 -0200

    Fixed a few issues on the rewritten graph data fetching code.

--- voyages/apps/voyage/graphs.py
+++ voyages/apps/voyage/graphs.py
@@ -1,7 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from itertools import groupby
 from voyages.apps.common.models import get_values_from_haystack_results
-from cache import VoyageCache
+from models import *
 import calendar
 
 class Axis:
@@ -23,25 +23,25 @@ class Axis:
         """
         Override this method in sub-classes so that
         the values can be sorted by this key.
-        :param d:
-        :return:
+        :param d: The dictionary representing the data point
+        :return: The value used to group data points on this axis.
         """
         return self.get_value(d)
 
     def get_stat(self, lst):
         """
         Computes an aggregate value over the given list.
-        :param lst: The list of values.
+        :param lst: The list of data points.
         :return: The aggregate result.
         """
         if self.mode is None:
-            return 0
+            return None
         values = [b.get(self.var_name) for b in lst]
         count = sum([0.0 if x is None else 1.0 for x in values])
         if self.mode == Axis.COUNT_MODE:
             return count
         if count == 0:
-            return 0
+            return None
         if self.mode == Axis.SUM_MODE:
             return sum([0.0 if x is None else float(x) for x in values])
         if self.mode == Axis.AVERAGE_MODE:
@@ -73,12 +73,8 @@ class ForeignKeyAxis(Axis):
         return self.related_objects.get(key)
 
 class MonthAxis(Axis):
-    CSV_FORMAT = 'csv'
-    ISO_FORMAT = 'iso'
-
-    def __init__(self, var_name, description, mode=None, format=ISO_FORMAT):
+    def __init__(self, var_name, description, mode=None):
         Axis.__init__(self, var_name + '_month', description, mode)
-        self.format = format
 
     def get_group_key(self, d):
         month = d.get(self.var_name)
@@ -96,6 +92,8 @@ class PercentageAxis(Axis):
 
     def get_stat(self, lst):
         result = Axis.get_stat(self, lst)
+        if result is None:
+            return None
         return round(result * 100, 1)
 
 class YearRangeAxis(Axis):
@@ -143,26 +141,35 @@ graphs_y_axes = [Axis('var_voyage_id', 'Number of voyages', Axis.COUNT_MODE),
                  PercentageAxis('var_imputed_percentage_male', 'Percentage male*'),
                  Axis('var_imputed_sterling_cash', 'Sterling cash price in Jamaica*', Axis.AVERAGE_MODE),
                  Axis('var_resistance', 'Rate of resistance', Axis.FREQUENCY_MODE),
-                 Axis('var_imputed_mortality',
-                      'Percentage of slaves embarked who died during voyage*',
-                      Axis.AVERAGE_MODE)]
+                 PercentageAxis('var_imputed_mortality', 'Percentage of slaves embarked who died during voyage*')]
 
-VoyageCache.load()
-_ports = {p.value: p.name for p in VoyageCache.ports.values()}
-_regions = {r.value: r.name for r in VoyageCache.regions.values()}
-_broad_regions = {b.value: b.name for b in VoyageCache.broad_regions.values()}
+# TODO: value SHOULD be used as the pk for several of our Django models.
+# Once this is in place, we can use VoyageCache to store this data.
+def cache_labels(model, label_field='label'):
+    return {x[0]: _(x[1]) for x in model.objects.values_list('value', label_field).iterator()}
 
-other_graphs_x_axes = [ForeignKeyAxis('var_imputed_nationality', 'Flag*', VoyageCache.nations),
-                       ForeignKeyAxis('var_rig_of_vessel', 'Rig', VoyageCache.rigs),
+_ports = cache_labels(Place, 'place')
+_regions = cache_labels(Region, 'region')
+_broad_regions = cache_labels(BroadRegion, 'broad_region')
+_nations = cache_labels(Nationality)
+_rigs = cache_labels(RigOfVessel)
+_particular_outcomes = cache_labels(ParticularOutcome)
+_slave_outcomes = cache_labels(SlavesOutcome)
+_owner_outcomes = cache_labels(OwnerOutcome)
+_captured_outcomes = cache_labels(VesselCapturedOutcome)
+_resistances = cache_labels(Resistance)
+
+other_graphs_x_axes = [ForeignKeyAxis('var_imputed_nationality', 'Flag*', _nations),
+                       ForeignKeyAxis('var_rig_of_vessel', 'Rig', _rigs),
                        ForeignKeyAxis('var_outcome_voyage',
                                       'Particular outcome of the voyage',
-                                      VoyageCache.particular_outcomes),
-                       ForeignKeyAxis('var_outcome_slaves', 'Outcome for slaves*', VoyageCache.slave_outcomes),
-                       ForeignKeyAxis('var_outcome_owner', 'Outcome for owner*', VoyageCache.owner_outcomes),
+                                      _particular_outcomes),
+                       ForeignKeyAxis('var_outcome_slaves', 'Outcome for slaves*', _slave_outcomes),
+                       ForeignKeyAxis('var_outcome_owner', 'Outcome for owner*', _owner_outcomes),
                        ForeignKeyAxis('var_outcome_ship_captured',
                                       'Outcome if ship captured*',
-                                      VoyageCache.captured_outcomes),
-                       ForeignKeyAxis('var_resistance', 'African resistance', VoyageCache.resistances),
+                                      _captured_outcomes),
+                       ForeignKeyAxis('var_resistance', 'African resistance', _resistances),
                        ForeignKeyAxis('var_imp_port_voyage_begin', 'Place where voyage began*', _ports),
                        ForeignKeyAxis('var_imp_region_voyage_begin', 'Region where voyage began*', _regions),
                        ForeignKeyAxis('var_imp_principal_place_of_slave_purchase',
@@ -203,16 +210,16 @@ def get_graph_data(search_query_set, x_axis, y_axes):
     """
     Obtain the graph data for the given axes extracted from
     the SearchQuerySet results.
-    :param results: A SearchQuerySet.
+    :param search_query_set: A SearchQuerySet object.
     :param x_axis: The X-Axis.
     :param y_axes: A list of axes that will be plotted along the Y-axis.
     :return: A dictionary containing an entry for each element in
     y_axes (with key equal to the axis description).
     The dictionary values are 2D point arrays, representing (x, y) values.
     """
-    # Fetch only the required data from the search query set.
     if len(y_axes) == 0:
         return {}
+    # Fetch only the required data from the search query set.
     fields = [y_axis.var_name for y_axis in y_axes]
     fields.append(x_axis.var_name)
     data = get_values_from_haystack_results(search_query_set, fields)
@@ -226,5 +233,8 @@ def get_graph_data(search_query_set, x_axis, y_axes):
         x_val = x_axis.get_value(lst[0])
         for y_axis in y_axes:
             y_val = y_axis.get_stat(lst)
+            # We may omit groups which cannot compute their statistic.
+            if y_val is None:
+                continue
             result[y_axis.description].append((x_val, y_val))
     return result
