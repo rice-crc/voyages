410231dcc279e0e69ddd179978f340a045cec029
Domingos <domingos.junior@gmail.com>
2017-03-10 17:13:03 -0300

    Added unique indices to Region and BroadRegion value fields (numerical codes).

---
+++ voyages/apps/voyage/migrations/0008_auto_20170310_1505.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('voyage', '0007_auto_20170302_1325'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='broadregion',
+            name='value',
+            field=models.IntegerField(unique=True, verbose_name=b'Numeric code'),
+        ),
+        migrations.AlterField(
+            model_name='region',
+            name='value',
+            field=models.IntegerField(unique=True, verbose_name=b'Numeric code'),
+        ),
+    ]
