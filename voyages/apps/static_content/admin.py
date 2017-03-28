1c1ba30f9466092262fa6fefd44f93c5e19120eb
Alex Thomas <athom09@emory.edu>
2017-03-28 15:53:15 -0400

    Moved Flatpage config to more appropriate place. Unlocked Static groups

--- voyages/apps/static_content/admin.py
+++ voyages/apps/static_content/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import ContentGroup, ContentPage
-from django.contrib.flatpages.admin import FlatPageAdmin
-from django.contrib.flatpages.models import FlatPage
-from django.utils.translation import ugettext_lazy as _
+
 
 
 class ContentGroupAdmin(admin.ModelAdmin):
@@ -15,30 +13,10 @@ class ContentPageAdmin(admin.ModelAdmin):
     readonly_fields = ["order", "group"]
     search_fields = ["group__name"]
 
-    # Lock adding new items
-    def has_add_permission(self, request):
-        return False
+    # # Lock adding new items
+    # def has_add_permission(self, request):
+    #     return False
 
 
-
-# Define a new FlatPageAdmin
-class FlatPageAdmin(FlatPageAdmin):
-    fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
-        (_('Advanced options'), {
-            'classes': ('collapse', ),
-            'fields': (
-                'registration_required',
-                'template_name',
-            ),
-        }),
-    )
-
-# Group remains closed in the admin
-# admin.site.register(ContentGroup, ContentGroupAdmin)
+admin.site.register(ContentGroup, ContentGroupAdmin)
 admin.site.register(ContentPage, ContentPageAdmin)
-
-
-# Re-register FlatPageAdmin
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
