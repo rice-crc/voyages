e48c7909bb0e998a5fec18a3b9db4af10a8018e4
Domingos <domingos.junior@gmail.com>
2017-04-09 16:23:07 -0300

    Beta UI now uses DataTables and AJAX to fetch the search results.

--- voyages/apps/voyage/beta_views.py
+++ voyages/apps/voyage/beta_views.py
@@ -1,4 +1,93 @@
+from django.core.paginator import Paginator
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from haystack.query import SearchQuerySet
+from search_indexes import VoyageIndex
+from voyages.apps.voyage.models import *
+import json
+
+class SearchOperator():
+    def __init__(self, front_end_op_str, back_end_op_str, list_type):
+        self.front_end_op_str = front_end_op_str
+        self.back_end_op_str = back_end_op_str
+        self.list_type = list_type
+
+# A list of operators used with Solr/Haystack to perform searches.
+_operators_list = [
+    SearchOperator('equals', 'exact', False),
+    SearchOperator('is at most', 'lte', False),
+    SearchOperator('is at least', 'gte', False), 
+    SearchOperator('is between', 'range', True),
+    SearchOperator('contains', 'contains', False),
+]
+_operators_dict = {op.front_end_op_str: op for op in _operators_list}
+
+index = VoyageIndex()
+plain_text_suffix = '_plaintext'
+plain_text_suffix_list = [f[:-len(plain_text_suffix)] for f in index.fields.keys() if f.endswith(plain_text_suffix)]
+translate_suffix = '_lang_en'
+translated_field_list = [f[:-len(translate_suffix)] for f in index.fields.keys() if f.endswith(translate_suffix)]
+
+def perform_search(search, lang):
+    items = search['items']
+    search_terms = {}
+    for item in items:
+        term = item['searchTerm']
+        operator = _operators_dict[item['op']]
+        is_list = isinstance(term, list)
+        if is_list and not operator.list_type:
+            term = term[0]
+        search_terms[u'var_' + unicode(item['varName']) + u'__' + unicode(operator.back_end_op_str)] = term
+    print search_terms
+    result = SearchQuerySet().models(Voyage).filter(**search_terms)
+    order_fields = search.get('orderBy')
+    if order_fields:
+        remaped_fields = []
+        for field in order_fields:
+            # Remap field names if they are plain text or language dependent.
+            order_by_field = u'var_' + unicode(field['name'])
+            if order_by_field in translated_field_list:
+                order_by_field += '_lang_' + lang + '_exact'
+            elif order_by_field in plain_text_suffix_list:
+                order_by_field += '_plaintext_exact'
+            if field['direction'] == 'desc':
+                order_by_field = '-' + order_by_field
+        result = result.order_by(*remaped_fields)
+    return result
+
+def get_results_table(results, post):
+    # Here we output a data set page.
+    table_params = post['tableParams']
+    rows_per_page = int(table_params['length'])
+    current_page_num = 1 + int(table_params['start']) / rows_per_page
+    paginator = Paginator(results, rows_per_page)
+    page = paginator.page(current_page_num)
+    reponse_data = {}
+    total_results = results.count()
+    reponse_data['recordsTotal'] = total_results
+    reponse_data['recordsFiltered'] = total_results
+    reponse_data['draw'] = int(table_params['draw'])
+    reponse_data['data'] = [x.get_stored_fields() for x in page]
+    return reponse_data
+
+@require_POST
+def ajax_search(request):
+    #try:
+    data = json.loads(request.body)
+    search = data['searchData']
+    lang = request.LANGUAGE_CODE
+    results = perform_search(search, lang)
+    # The output now depends on which type of
+    # result the caller expects.
+    response_data = {}
+    if data['output'] == 'resultsTable':
+        response_data = get_results_table(results, data)
+    else:
+        return HttpResponseBadRequest('Unkown type of output.')
+    return JsonResponse(response_data)
+    #except Exception as e:
+    #    return HttpResponseBadRequest(str(e))
 
 def search_view(request):
     return render(request, 'voyage/beta_search_main.html')
