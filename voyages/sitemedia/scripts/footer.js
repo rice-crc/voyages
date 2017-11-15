5ed2bfcca19ee91bce33d2581e2325307d1b348c
Yang <yang@truman.edu>
2017-11-15 10:31:29 -0500

    make footer sticky

--- voyages/sitemedia/scripts/footer.js
+++ voyages/sitemedia/scripts/footer.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  var offcanvasHeight = $(".sidebar-offcanvas").height();
-  if (offcanvasHeight > $(".container-new").height()) {
-    $(".container-new").height(offcanvasHeight);
-  }
+  $('[data-toggle="offcanvas"]').click(function() {
+    $('.row-offcanvas').toggleClass('active')
+  });
 });
 
 $(window).resize(function() {
+  var footerHeight = $("footer").height();
+  $("body").css("margin-bottom", footerHeight+"px");
+});
+
+$(document).ready(function() {
   var offcanvasHeight = $(".sidebar-offcanvas").height();
   if (offcanvasHeight > $(".container-new").height()) {
     $(".container-new").height(offcanvasHeight);
