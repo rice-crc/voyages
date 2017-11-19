62aca5a9b8c0218388b5ca7ba7fc0a8fdeee63fe
Yang <yang@truman.edu>
2017-11-19 18:20:17 -0500

    migrate help pages to about pages

--- voyages/apps/about/urls.py
+++ voyages/apps/about/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     url(r'^history', TemplateView.as_view(template_name='about/history.html'), name='history'),
     url(r'^team', TemplateView.as_view(template_name='about/team.html'), name='team'),
     url(r'^data', TemplateView.as_view(template_name='about/data.html'), name='data'),
+    url(r'^faq', TemplateView.as_view(template_name='about/faq.html'), name='faq'),
+    url(r'^glossary', TemplateView.as_view(template_name='about/glossary.html'), name='glossary'),
+    url(r'^legal', TemplateView.as_view(template_name='about/legal.html'), name='legal'),
+
     url(r'^acknowledgements',
         TemplateView.as_view(template_name='about/acknowledgements.html'), name='acknowledgements'),
     url(r'^origins', TemplateView.as_view(template_name='about/origins.html'), name='origins'),
