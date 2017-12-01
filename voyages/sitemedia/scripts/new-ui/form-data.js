33dfa46efeddff4d8a8ba9d53f35d110691df739
Yang <yang@truman.edu>
2017-12-01 11:39:08 -0500

    refactor

---
+++ voyages/sitemedia/scripts/new-ui/form-data.js
@@ -0,0 +1,37 @@
+// form data //
+// flag
+var flagData = [
+	{id:0, text:'Spain', value:"1"},
+	{id:1, text:'Uruguay', value:"1"},
+	{id:2, text:'Portugal', value:"1"},
+	{id:3, text:'Brazil', value:"1"},
+	{id:4, text:'Great Britain', value:"1"},
+	{id:5, text:'Netherlands', value:"1"},
+	{id:6, text:'U.S.A', value:"1"},
+	{id:7, text:'France', value:"1"},
+	{id:8, text:'Denmark', value:"1"},
+	{id:9, text:'Hanse Towns, Brandenburg', value:"1"},
+	{id:10, text:'Sweden', value:"1"},
+	{id:11, text:'Norway', value:"1"},
+	{id:12, text:'Argentina', value:"1"},
+	{id:13, text:'Russia', value:"1"},
+	{id:14, text:'Sardinia', value:"1"},
+	{id:15, text:'Mexico', value:"1"},
+	{id:16, text:'Genoa', value:"1"},
+	{id:17, text:'Duchy of Courland', value:"1"},
+	{id:18, text:'Prussia', value:"1"},
+	{id:19, text:'Bremen', value:"1"},
+	{id:20, text:'Other', value:"1"}
+]
+
+// flag-imputed
+var flagImputedData = [
+	{id:0, text:'Spain/Uruguay', value:"1"},
+	{id:1, text:'Portugal/Brazil', value:"1"},
+	{id:2, text:'Great Britain', value:"1"},
+	{id:3, text:'Netherlands', value:"1"},
+	{id:4, text:'U.S.A', value:"1"},
+	{id:5, text:'France', value:"1"},
+	{id:6, text:'U.S.A', value:"1"},
+	{id:7, text:'Denmark/Baltic', value:"1"}
+]
