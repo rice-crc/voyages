ac6e40813dc6713b09f62c8115828b94abcea642
Domingos <domingos.junior@gmail.com>
2017-12-02 11:03:16 -0200

    Adding a new version of breadcrumbs to be used with new UI. Adding a translation command to the app that will generate a POT file based on a predetermined list of fields that are fetched from the database. Removing ember files.

--- ember/app/services/ajax.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import AjaxService from 'ember-ajax/services/ajax';
-
-export default AjaxService.extend({
-  host: 'http://localhost:8000'
-});
---
+++ voyages/apps/voyage/management/commands/translation.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from voyages.apps.voyage.models import *
+from datetime import datetime
+
+class Command(BaseCommand):
+    help = 'Extract text from the database that needs to be translated.'
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fieldsets = {
+            VoyageSourcesType: ['group_name'],
+            ParticularOutcome: ['label'],
+            SlavesOutcome: ['label'],
+            VesselCapturedOutcome: ['label'],
+            OwnerOutcome: ['label'],
+            Resistance: ['label'],
+        }
+
+    def handle(self, *args, **options):
+        data = []
+        for k, fields in self.fieldsets.items():
+            for field in fields:
+                data += k.objects.values_list(field, flat=True)
+        data = sorted(set(data))
+        # POT Header.
+        print('# Auto-generated file based on entries in the voyage database')
+        print('#')
+        print('msgid ""')
+        print('msgstr ""')
+        print('"Project-Id-Version: slave-voyages.org\\n"')
+        print('"POT-Creation-Date: ' + datetime.now().strftime("%Y-%m-%d %H:%M") + '\\n"')
+        print('"PO-Revision-Date: ' + datetime.now().strftime("%Y-%m-%d %H:%M") + '\\n"')
+        print('"Last-Translator: ?\\n"')
+        print('"Language-Team: Voyages\\n"')
+        print('"MIME-Version: 1.0\\n"')
+        print('"Content-Type: text/plain; charset=UTF-8\\n"')
+        print('"Content-Transfer-Encoding: 8bit\\n"')
+        for s in data:
+            print('')
+            print('msgid "' + s + '"')
+            print('msgstr ""')
