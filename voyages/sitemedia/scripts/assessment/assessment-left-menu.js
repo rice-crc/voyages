d1fa458422265c655a66f073ea17f24fa091d85f
Yang <yang@truman.edu>
2017-12-17 20:09:36 -0500

    [BUG] Restore functionality of search buttons on the assessment estimate page

--- voyages/sitemedia/scripts/assessment/assessment-left-menu.js
+++ voyages/sitemedia/scripts/assessment/assessment-left-menu.js
@@ -79,17 +79,18 @@ function areaClick(clicked_input){
 }
 
 function allClicked(clicked_button, action){
-    var div_parent = $(clicked_button).parents().eq(4);
+    var div_parent = $(clicked_button).parents().eq(0);
     if (action == 1){
         $(div_parent).find("input[name^=darea-button-]").prop("checked", true);
         $(div_parent).find("input[name^=earea-button-]").prop("checked", true);
         $(div_parent).find("input[name^=dregion-button-]").prop("checked", true);
         $(div_parent).find("input[name^=eregion-button-]").prop("checked", true);
     } else {
-        $(div_parent).find("input[name^=darea-button-]").prop("checked", false);
-        $(div_parent).find("input[name^=earea-button-]").prop("checked", false);
-        $(div_parent).find("input[name^=dregion-button-]").prop("checked", false);
-        $(div_parent).find("input[name^=eregion-button-]").prop("checked", false);
+      debugger;
+      $(div_parent).find("input[name^=darea-button-]").prop("checked", false);
+      $(div_parent).find("input[name^=earea-button-]").prop("checked", false);
+      $(div_parent).find("input[name^=dregion-button-]").prop("checked", false);
+      $(div_parent).find("input[name^=eregion-button-]").prop("checked", false);
     }
 
     return false;
