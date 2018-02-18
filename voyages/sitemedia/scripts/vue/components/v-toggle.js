b461231cebfd6040a9e4272fae86e90d900e9731
Yang <yang@truman.edu>
2018-02-18 14:13:15 -0500

    wrap up search settings and v-toggle component

--- voyages/sitemedia/scripts/vue/components/v-toggle.js
+++ voyages/sitemedia/scripts/vue/components/v-toggle.js
@@ -1,13 +1,24 @@
 Vue.component('v-toggle', {
-  props: ['toggled','label'],
+  props: ['title', 'description', 'toggled', 'label'],
   template: `
-    <div>
-      {{ label }}
-      <span class="fa toggle fa-control"
-            v-bind:class="{'fa-toggle-on': toggled, 'fa-toggle-off': !toggled, 'primary-color': toggled, 'text-muted': !toggled}"
-            @click="click">
-      </span>
-      <span class="toggle-label" v-show="label"></span>
+    <div class="v-form-group">
+      <div class="v-title">
+        <span>{{title}}</span>
+        <span>
+          <span class="fa toggle fa-control"
+                v-bind:class="{'fa-toggle-on': toggled, 'fa-toggle-off': !toggled, 'primary-color': toggled, 'text-muted': !toggled}"
+                @click="click">
+          </span>
+          <span class="toggle-label" v-show="label"></span>
+        </span><!-- reserved for right aligned content -->
+      </div>
+      <div class="v-description" v-text="description"></div>
+
+      <div class="row v-padding">
+        <div class="col-md-12">
+          <code>toggled: {{toggled}}</code>
+        </div>
+      </div>
     </div>
   `,
   methods: {
