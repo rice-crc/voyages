57acb55c878e9883decb068b317686d823ec1ba3
Yang <yang@truman.edu>
2018-02-18 09:58:01 -0500

    add landing numbers

---
+++ voyages/sitemedia/scripts/vue/variables/slave.js
@@ -0,0 +1,153 @@
+slave = {
+  overallNumbers: {
+    var_imp_total_num_slaves_purchased: {
+      varName: 'var_imp_total_num_slaves_purchased',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_total_num_slaves_purchased: {
+      varName: 'var_total_num_slaves_purchased',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_imp_total_slaves_disembarked: {
+      varName: 'var_imp_total_slaves_disembarked',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  purchaseNumbers: {
+    var_num_slaves_intended_first_port: {
+      varName: 'var_num_slaves_intended_first_port',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    var_num_slaves_carried_first_port: {
+      varName: 'var_num_slaves_carried_first_port',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    var_num_slaves_carried_second_port: {
+      varName: 'var_num_slaves_carried_second_port',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    var_num_slaves_carried_third_port: {
+      varName: 'var_num_slaves_carried_third_port',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  landingNumbers: {
+    var_total_num_slaves_arr_first_port_embark: {
+      varName: 'var_total_num_slaves_arr_first_port_embark',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    var_num_slaves_disembark_first_place: {
+      varName: 'var_num_slaves_disembark_first_place',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    var_num_slaves_disembark_second_place: {
+      varName: 'var_num_slaves_disembark_second_place',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    var_num_slaves_disembark_third_place: {
+      varName: 'var_num_slaves_disembark_third_place',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  percentageBySexAndAgeGroup: {
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+  otherCharacteristics: {
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  count: {
+    changed: 0,
+    activated: 0,
+  },
+}
