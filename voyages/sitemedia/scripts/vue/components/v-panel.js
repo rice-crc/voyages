b61b75460bf802ebaeeb54c72b51af5430a825a5
Yang <yang@truman.edu>
2018-02-18 12:28:32 -0500

    refactor panel-header

--- voyages/sitemedia/scripts/vue/components/v-panel.js
+++ voyages/sitemedia/scripts/vue/components/v-panel.js
@@ -1,6 +1,6 @@
 // v-panel
 Vue.component('v-panel', {
-  props: ['title', "isAdvanced", "filters", "count", "group", "subGroup"],
+  props: ['title', "count", "filters", "isAdvanced", "group", "subGroup"],
   template: `
     <li v-if="isAdvancedValue" class="dropdown-item dropdown-item-li search-dropdown-item" :data-submenu-id="idValue">
         <div class="dropdown-menu-title">
@@ -65,84 +65,3 @@ Vue.component('v-panel', {
 
 })
 // v-panel
-
-// v-panel-header
-Vue.component('v-panel-header', {
-  props: ['title', "description", "count"],
-  template: `
-    <div class="v-panel-header">
-      <div class="v-panel-title-container">
-        <div class="v-panel-title">
-        {{title}}
-        </div>
-        <div class="v-panel-counter">
-          <div class="text-center">
-          <!--
-            <b-button variant="info" size="sm">
-              Parameters Applied <b-badge variant="light">{{count}}</b-badge>
-            </b-button>
-          -->
-
-          <b-btn variant="outline-info" size="sm" v-b-modal.modal-center>
-            <i class="fa fa-question-circle-o"></i>
-            Help
-          </b-btn>
-          <b-modal id="modal-center" centered title="modalTitle">
-            <slot name="v-modal-content"></slot>
-          </b-modal>
-
-          </div>
-        </div>
-      </div>
-      <div class="v-panel-description" v-text="description"></div>
-
-    </div>
-  `,
-
-  data: function() {
-    return {
-      titleValue: '',
-      descriptionValue: '',
-      countValue: null,
-      modalTitle: '',
-    }
-  },
-
-  methods: {
-    emitParent: function() {
-      this.$emit('blurred', this.textboxValue);
-    }
-  },
-
-  watch: {
-    title: { // this is the value from props
-      handler: function(value) {
-        this.titleValue = value; // titleValue is the local copy used in the child component
-      }
-    },
-    description: {
-      handler: function(value) {
-        this.descriptionValue = value; // descriptionValue is the local copy used in the child component
-      }
-    },
-    count: {
-      handler: function(value) {
-        this.countValue = value; // countValue is the local copy used in the child component
-      }
-    },
-    filters: {
-
-    }
-
-
-  },
-
-  mounted: function() { // load value initially
-    this.titleValue = this.title;
-    this.descriptionValue = this.description;
-    this.countValue = this.count;
-    this.modalTitle = "Help about " + this.title;
-  }
-
-})
-// v-panel-header
