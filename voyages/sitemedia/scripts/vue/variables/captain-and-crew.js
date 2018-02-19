aabee58c8c2219dcc50ce118964806b10ee606ad
Yang <yang@truman.edu>
2018-02-18 21:54:11 -0500

    add captain and crew

---
+++ voyages/sitemedia/scripts/vue/variables/captain-and-crew.js
@@ -0,0 +1,63 @@
+captainAndCrew = {
+  captain: {
+    var_captain_plaintext: {
+      varName: 'captain_plaintext',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  crew: {
+    var_crew_voyage_outset: {
+      varName: 'crew_voyage_outset',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_crew_first_landing: {
+      varName: 'crew_first_landing',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_crew_died_complete_voyage: {
+      varName: 'crew_died_complete_voyage',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  count: {
+    changed: 0,
+    activated: 0,
+  },
+}
