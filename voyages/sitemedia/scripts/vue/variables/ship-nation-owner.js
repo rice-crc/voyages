b623d50b1b72ece3cc96c9cc199713fc0441af09
Yang <yang@truman.edu>
2018-02-18 21:15:48 -0500

    add additional ship nation owner

--- voyages/sitemedia/scripts/vue/variables/ship-nation-owner.js
+++ voyages/sitemedia/scripts/vue/variables/ship-nation-owner.js
@@ -50,6 +50,108 @@ shipNationOwner = {
     }
   },
 
+  constructionAndRegistration: {
+    var_year_of_construction: {
+      varName: 'year_of_construction',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_vessel_construction_place: {
+      varName: 'vessel_construction_place',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_registered_year: {
+      varName: 'registered_year',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_registered_place: {
+      varName: 'registered_place',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
+  rigTonnageAndGunsMounted: {
+    var_rig_of_vessel_plaintext: {
+      varName: 'rig_of_vessel_plaintext',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_tonnage: {
+      varName: 'tonnage',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_tonnage_mod: {
+      varName: 'tonnage_mod',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    var_guns_mounted: {
+      varName: 'guns_mounted',
+      value: {
+        op: "equals to",
+        searchTerm0: null,
+        searchTerm1: null,
+      },
+      changed: false,
+      activated: false,
+    },
+
+    count: {
+      changed: 0,
+      activated: 0,
+    }
+  },
+
   count: {
     changed: 0,
     activated: 0,
