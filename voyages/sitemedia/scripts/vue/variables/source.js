c92bad26a9c91025f5e6f1542bdf693d0b89323a
Yang <yang@truman.edu>
2018-02-22 11:22:22 -0500

    refactor source

--- voyages/sitemedia/scripts/vue/variables/source.js
+++ voyages/sitemedia/scripts/vue/variables/source.js
@@ -1,15 +1,18 @@
+var_sources_plaintext_search = new TextVariable({
+    varName: "sources_plaintext_search",
+    label: "Source",
+    description: "This is the source of the data. This could be a partial keyword.",
+  },{
+    op: "contains",
+    searchTerm: null,
+  },{
+    isImputed: false,
+    isAdvanced: false
+  });
+
 source = {
   source: {
-    var_sources_plaintext_search: {
-      varName: 'sources_plaintext_search',
-      value: {
-        op: "contains",
-        searchTerm0: null,
-        searchTerm1: null,
-      },
-      changed: false,
-      activated: false,
-    },
+    var_sources_plaintext_search: var_sources_plaintext_search,
     count: {
       changed: 0,
       activated: 0,
