fd82cfcca7365f4685b175ad1c608a84a4dfb904
Yang <yang@truman.edu>
2018-02-25 14:50:38 -0500

    add dropdown caret

--- voyages/sitemedia/scripts/vue/components/v-menu-title.js
+++ voyages/sitemedia/scripts/vue/components/v-menu-title.js
@@ -1,10 +1,11 @@
 // v-menu-title
 Vue.component('v-menu-title', {
-  props: ['title', "count"],
+  props: ['title', "count", "disableCaret"],
   template: `
     <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
       <slot name="menu-title">{{title}}</slot>
       <span v-if="count">({{count}})</span>
+      <i class="fa fa-caret-down" v-if="!disableCaret"></i>
     </a>
   `,
   data: function() {
