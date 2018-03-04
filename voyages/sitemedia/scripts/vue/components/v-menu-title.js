7fab36ffeaf14b37cc611a1d9183d85b0bbfa9c6
Yang <yang@truman.edu>
2018-03-04 09:37:26 -0500

    activate first row upon menu expansion

--- voyages/sitemedia/scripts/vue/components/v-menu-title.js
+++ voyages/sitemedia/scripts/vue/components/v-menu-title.js
@@ -2,7 +2,7 @@
 Vue.component('v-menu-title', {
   props: ['title', "count", "disableCaret"],
   template: `
-    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" @mouseover="activate" @click="activate">
       <slot name="menu-title">{{title}}</slot>
       <span v-if="count.activated">({{count.activated}})</span>
       <i class="fa fa-caret-down" v-if="!disableCaret"></i>
@@ -12,5 +12,15 @@ Vue.component('v-menu-title', {
     return {
     }
   },
+  methods: {
+    activate: function() {
+      // activate the first row by default upon expansion
+      this.$el.click();
+      var firstRow = this.$el.parentElement.lastElementChild.children[0].children[0];
+      if (this.$el.parentElement.lastElementChild.children[0].children[0].tagName == "LI") {
+        activateSubmenu(firstRow);
+      }
+    }
+  }
 })
 // v-menu-title
