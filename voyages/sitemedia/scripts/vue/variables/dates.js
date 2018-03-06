1a50f5ad5e0bd791724728af2d44d924788d0848
Yang <yang@truman.edu>
2018-03-06 01:38:33 -0500

    introduce new dates variables

--- voyages/sitemedia/scripts/vue/variables/dates.js
+++ voyages/sitemedia/scripts/vue/variables/dates.js
@@ -1,16 +1,3 @@
-var_imp_length_home_to_disembark = new NumberVariable({
-    varName: "imp_length_home_to_disembark",
-    label: "Voyage Length, Homeport to Slaves Landing (days)",
-    description: "Enter the number of crew at voyage outset. This could be a range.",
-  },{
-    op: "equals to",
-    searchTerm0: null,
-    searchTerm1: null
-  },{
-    isImputed: true,
-    isAdvanced: true
-  });
-
 var_length_middle_passage_days = new NumberVariable({
     varName: "length_middle_passage_days",
     label: "Middle Passage (days)",
@@ -24,10 +11,107 @@ var_length_middle_passage_days = new NumberVariable({
     isAdvanced: true
   });
 
+var_voyage_began = new NumberVariable({
+    varName: "voyage_began",
+    label: "Year Voyage Began",
+    description: "",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: true
+  });
+
+var_slave_purchase_began = new NumberVariable({
+    varName: "slave_purchase_began",
+    label: "Year Trade Began in Africa",
+    description: "",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: true
+  });
+
+var_date_departed_africa = new NumberVariable({
+    varName: "date_departed_africa",
+    label: "Year Vessel Departed Africa",
+    description: "",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: true
+  });
+
+var_first_dis_of_slaves = new NumberVariable({
+    varName: "first_dis_of_slaves",
+    label: "Year Vessel Arrived with Slaves",
+    description: "",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: true
+  });
+
+var_departure_last_place_of_landing = new NumberVariable({
+    varName: "departure_last_place_of_landing",
+    label: "Year Vessel Departed for Homeport",
+    description: "",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: true
+  });
+
+var_voyage_completed = new NumberVariable({
+    varName: "voyage_completed",
+    label: "Year Voyage Completed",
+    description: "",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: true
+  });
+
+var_imp_length_home_to_disembark = new NumberVariable({
+    varName: "imp_length_home_to_disembark",
+    label: "Voyage Length, Homeport to Slaves Landing (days)",
+    description: "Enter the number of crew at voyage outset. This could be a range.",
+  },{
+    op: "equals to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: true,
+    isAdvanced: true
+  });
+
 dates = {
   dates: {
-    var_imp_length_home_to_disembark: var_imp_length_home_to_disembark,
     var_length_middle_passage_days: var_length_middle_passage_days,
+    var_voyage_began: var_voyage_began,
+    var_slave_purchase_began: var_slave_purchase_began,
+    var_date_departed_africa: var_date_departed_africa,
+    var_first_dis_of_slaves: var_first_dis_of_slaves,
+    var_departure_last_place_of_landing: var_departure_last_place_of_landing,
+    var_voyage_completed: var_voyage_completed,
+    var_imp_length_home_to_disembark: var_imp_length_home_to_disembark,
     count: {
       changed: 0,
       activated: 0,
