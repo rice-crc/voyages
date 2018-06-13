9e9d0b119e0678629cc07686267e7fa302b8730d
Yang Li <yang@truman.edu>
2018-06-13 09:29:51 -0400

    update menuLength

--- voyages/sitemedia/scripts/vue/includes/helpers.js
+++ voyages/sitemedia/scripts/vue/includes/helpers.js
@@ -512,15 +512,15 @@ function refreshUi(filter, currentTab, tabData) {
       ],
 
       // page length Default
-      pageLength: 25,
+      pageLength: 20,
 
       // dom: 'ifrtBp',
       dom:  "<'flex-container'iB>" +
             "<'row'<'col-sm-12'tr>>" +
             "<'row'<'col-sm-5'><'col-sm-7'p>>",
       lengthMenu: [
-        [10, 25, 50, 100],
-        ['10 rows', '25 rows', '50 rows', '100 rows']
+        [20, 50, 100],
+        ['20 rows', '50 rows', '100 rows']
       ],
 
       language: {
