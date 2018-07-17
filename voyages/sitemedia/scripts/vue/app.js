1fe77f2318eede720e03fe11b69d391a46e8b303
Yang Li <yang@truman.edu>
2018-07-17 11:30:02 -0400

    try to add link to animation from front page

--- voyages/sitemedia/scripts/vue/app.js
+++ voyages/sitemedia/scripts/vue/app.js
@@ -341,3 +341,19 @@ var searchBar = new Vue({
     });
   },
 })
+
+
+
+window.onload = function(){
+  debugger;
+
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
+    //Change hash for page-reload
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').on('shown', function (e) {
+        window.location.hash = e.target.hash;
+    });
+}
