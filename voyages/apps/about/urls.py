4e03e76ca8cc7749e1c7daf13f24b09c9f6fe757
Yang Li <yang@truman.edu>
2018-08-22 12:46:15 -0400

    clean up flatpage translation for About

--- voyages/apps/about/urls.py
+++ voyages/apps/about/urls.py
@@ -6,6 +6,11 @@ import voyages.apps.about.views
 
 urlpatterns = [
     url(r'^$', voyages.apps.static_content.views.get_static_content, {'group': 'About'}, name='index'),
+    url(r'^(?P<flatpage_url>.*)', voyages.apps.about.views.render_about_flatpage), # Matches the keyword after about/* and map it to the flatpage
+    # Flatpage needs to have leading and trailing "/". For example "/about/history/pt/"
+
+    # Below is kept but not used because of links used in navigation
+    # TODO: Develop a dynamic menu
     url(r'^history', TemplateView.as_view(template_name='about/history.html'), name='history'),
     url(r'^team', TemplateView.as_view(template_name='about/team.html'), name='team'),
     url(r'^data', TemplateView.as_view(template_name='about/data.html'), name='data'),
@@ -16,36 +21,4 @@ urlpatterns = [
         TemplateView.as_view(template_name='about/acknowledgements.html'), name='acknowledgements'),
     url(r'^origins', TemplateView.as_view(template_name='about/origins.html'), name='origins'),
     url(r'^contacts', TemplateView.as_view(template_name='about/contacts.html'), name='contacts'),
-
-    # flatpage urls
-    url(r'^flatpage/index/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/index/en/'}),
-    url(r'^flatpage/index/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/index/pt/'}),
-    url(r'^flatpage/index/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/index/es/'}),
-    url(r'^flatpage/history/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/history/en/'}),
-    url(r'^flatpage/history/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/history/pt/'}),
-    url(r'^flatpage/history/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/history/es/'}),
-    url(r'^flatpage/team/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/team/en/'}),
-    url(r'^flatpage/team/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/team/pt/'}),
-    url(r'^flatpage/team/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/team/es/'}),
-    url(r'^flatpage/data/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/data/en/'}),
-    url(r'^flatpage/data/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/data/pt/'}),
-    url(r'^flatpage/data/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/data/es/'}),
-    url(r'^flatpage/faq/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/faq/en/'}),
-    url(r'^flatpage/faq/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/faq/pt/'}),
-    url(r'^flatpage/faq/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/faq/es/'}),
-    url(r'^flatpage/glossary/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/glossary/en/'}),
-    url(r'^flatpage/glossary/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/glossary/pt/'}),
-    url(r'^flatpage/glossary/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/glossary/es/'}),
-    url(r'^flatpage/legal/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/legal/en/'}),
-    url(r'^flatpage/legal/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/legal/pt/'}),
-    url(r'^flatpage/legal/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/legal/es/'}),
-    url(r'^flatpage/acknowledgements/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/acknowledgements/en/'}),
-    url(r'^flatpage/acknowledgements/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/acknowledgements/pt/'}),
-    url(r'^flatpage/acknowledgements/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/acknowledgements/es/'}),
-    url(r'^flatpage/origins/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/origins/en/'}),
-    url(r'^flatpage/origins/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/origins/pt/'}),
-    url(r'^flatpage/origins/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/origins/es/'}),
-    url(r'^flatpage/contacts/en', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/contacts/en/'}),
-    url(r'^flatpage/contacts/pt', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/contacts/pt/'}),
-    url(r'^flatpage/contacts/es', voyages.apps.about.views.render_about_flatpage, {'flatpage_url': '/about/contacts/es/'}),
 ]
