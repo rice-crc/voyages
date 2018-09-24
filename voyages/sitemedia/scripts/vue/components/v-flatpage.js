00a2a0a5831232f2c16d47f230d40bfa2f144c85
Yang Li <yang@truman.edu>
2018-09-24 17:35:49 -0400

    adjust style of flatpage indentation so that it doesn't span two lines

--- voyages/sitemedia/scripts/vue/components/v-flatpage.js
+++ voyages/sitemedia/scripts/vue/components/v-flatpage.js
@@ -7,9 +7,9 @@ Vue.component('v-flatpage', {
         <div class="sidebar-title-1">{{title}}</div>
       </div>
       <div class="sidebar-section">
-        <div v-for="item in list">
+        <div v-for="item in list" v-bind:class="[item.class, item.isActive ? 'article-list-active':''  ]">
           <a class="article-list-item" :id="item.id"
-            v-bind:class="[item.class, item.isActive ? 'article-list-active':''  ]"  @click=click>{{item.title}}</a>
+              @click=click>{{item.title}}</a>
         </div>
       </div>
     </div>
