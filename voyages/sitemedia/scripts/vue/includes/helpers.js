a231a869e27f4dd6222a00f8cd12ad65783d5b8b
Yang Li <yang@truman.edu>
2018-10-08 13:53:17 -0400

    change default rows in table to 15

--- voyages/sitemedia/scripts/vue/includes/helpers.js
+++ voyages/sitemedia/scripts/vue/includes/helpers.js
@@ -575,15 +575,15 @@ function refreshUi(filter, currentTab, tabData) {
       "destroy": true,
 
       // page length Default
-      pageLength: 20,
+      pageLength: 15,
 
       // dom: 'ifrtBp',
       dom: "<'flex-container'iB>" +
         "<'row'<'col-sm-12'tr>>" +
         "<'row'<'col-sm-5'><'col-sm-7'p>>",
       lengthMenu: [
-        [20, 50, 100, 200],
-        ['20 rows', '50 rows', '100 rows', '200 rows']
+        [15, 50, 100, 150],
+        ['15 rows', '50 rows', '100 rows', '200 rows']
       ],
 
       language: {
