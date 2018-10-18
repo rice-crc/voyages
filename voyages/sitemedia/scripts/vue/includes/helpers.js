a9e8f614ddb02e180304ef224700333b326d916d
Yang Li <yang@truman.edu>
2018-10-18 08:02:28 -0400

    bring back dt download/export

--- voyages/sitemedia/scripts/vue/includes/helpers.js
+++ voyages/sitemedia/scripts/vue/includes/helpers.js
@@ -635,33 +635,33 @@ function refreshUi(filter, filterData, currentTab, tabData) {
       buttons: [
         columnToggleMenu,
         pageLength,
-        // {
-        //   extend: 'collection',
-        //   // text: '<span class="fa fa-columns" style="vertical-align: middle;"></span>',
-        //   className: 'btn btn-info buttons-collection dropdown-toggle',
-        //   text: 'Download',
-        //   titleAttr: 'Download results',
-        //   buttons: [
-        //     // {
-        //     // 	text: 'CSV - not implemented',
-        //     // 	action: function() { alert('not implemented yet'); },
-        //     // },
-        //     {
-        //       text: 'Excel',
-        //       action: function() {
-        //         var visibleColumns = $.map($.makeArray(mainDatatable.columns().visible()), function(visible, index) {
-        //           return visible ? allColumns[index].data : undefined;
-        //         });
-        //         var form = $("<form action='876167cf-bc40-44f7-9557-ee8117d94008/beta_ajax_download' method='post'>{% csrf_token %}</form>");
-        //         form.append($("<input name='data' type='hidden'></input>").attr('value', JSON.stringify({
-        //           searchData: currentSearchObj,
-        //           cols: visibleColumns
-        //         })));
-        //         form.appendTo('body').submit().remove();
-        //       },
-        //     }
-        //   ]
-        // }
+        {
+          extend: 'collection',
+          text: '<span class="fa fa-columns" style="vertical-align: middle;"></span>',
+          className: 'btn btn-info buttons-collection dropdown-toggle',
+          text: 'Download',
+          titleAttr: 'Download results',
+          buttons: [
+            {
+            	text: 'CSV - not implemented',
+            	action: function() { alert('not implemented yet'); },
+            },
+            {
+              text: 'Excel',
+              action: function() {
+                var visibleColumns = $.map($.makeArray(mainDatatable.columns().visible()), function(visible, index) {
+                  return visible ? allColumns[index].data : undefined;
+                });
+                var form = $("<form action='876167cf-bc40-44f7-9557-ee8117d94008/beta_ajax_download' method='post'>{% csrf_token %}</form>");
+                form.append($("<input name='data' type='hidden'></input>").attr('value', JSON.stringify({
+                  searchData: currentSearchObj,
+                  cols: visibleColumns
+                })));
+                form.appendTo('body').submit().remove();
+              },
+            }
+          ]
+        }
       ],
       //pagingType: "input",
       bFilter: false,
