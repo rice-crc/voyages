464be971a1e1e3981d4c8b9afd97c327eea50b0f
Yang Li <yang@truman.edu>
2019-01-30 12:02:15 -0500

    remove obsolete code from about app views.py

--- voyages/apps/about/views.py
+++ voyages/apps/about/views.py
@@ -1,11 +1,5 @@
 from voyages.apps.common.views import render_locale_flatpage
 
-# def render_about_flatpage(request, flatpage_url):
-#     """
-#     Renders flat pages using the default template for About pages.
-#     """
-#     return render_locale_flatpage(request, 'about/flatpage.html', flatpage_url)
-
 # A catch all for URL matching
 def render_about_flatpage(request, flatpage_url):
     flatpage_url = "/about/" + flatpage_url + "/"
