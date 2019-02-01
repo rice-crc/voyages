03b5bd9ec54ddb496ba9df1bd579990e64eb134b
Yang Li <yang@truman.edu>
2019-02-01 05:01:52 -0500

    add two videos (placeholder)

---
+++ voyages/sitemedia/scripts/vue/components/v-flatpage-ship.js
@@ -0,0 +1,18 @@
+// intialization of data object for voyage/ship flatpage structure
+data = {
+  title: {},
+  response: {},
+  articles: [],
+  breadcrumb: [],
+  content: "",
+  pathname: "ship",
+  previous: {
+    title: "",
+    index: 0,
+  },
+  next: {
+    title: "",
+    index: 0,
+  },
+  currentIndex: 0,
+}
