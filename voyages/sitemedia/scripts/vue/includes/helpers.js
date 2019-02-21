5efe8e9bc0b189844bad896def024ec7d66e012a
Yang Li <yang@truman.edu>
2019-02-21 13:37:55 -0500

    reorder the columns by a specific order (not sure if this is the best way)

--- voyages/sitemedia/scripts/vue/includes/helpers.js
+++ voyages/sitemedia/scripts/vue/includes/helpers.js
@@ -656,13 +656,25 @@ function refreshUi(filter, filterData, currentTab, tabData, options) {
       },
       
       scrollX: true,
+      colReorder: {
+        order: [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+          11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 0,
+          31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+          41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+          51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+          61, 62
+        ]
+      },
+
       columnDefs: [{
           width: "1%",
           targets: 0
         },
         {
-          width: "10%",
-          targets: 3
+          width: "5%",
+          targets: 1
         },
         {
           width: "15%",
@@ -767,7 +779,6 @@ function refreshUi(filter, filterData, currentTab, tabData, options) {
       columns: allColumns,
       stateSave: true,
       stateDuration: -1,
-      colReorder: true,
       initComplete: function() {
         $('[data-toggle="tooltip"]').tooltip()
       }
