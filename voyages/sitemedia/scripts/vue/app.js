6a5f72f46047d5fab9e015c95c49fd46cbe7dddd
Yang Li <yang@truman.edu>
2019-03-25 12:56:19 -0400

    do not remove saved searches during a reset

--- voyages/sitemedia/scripts/vue/app.js
+++ voyages/sitemedia/scripts/vue/app.js
@@ -349,7 +349,7 @@ var searchBar = new Vue({
     resetAll() {
       window.location.reload(); // forced reload
       this.resetURL();
-      localStorage.removeItem("saved");
+      // localStorage.removeItem("saved");
       // for (group in this.filter) {
       //   if (group !== "settings") {
       //     for (subGroup in this.filter[group]) {
