2e6161c99ad0cb6889e88a6c9ebcd5e46c8d72d0
Yang Li <yang@truman.edu>
2019-04-05 17:03:58 -0400

    refactor url, views, and helpers.js to remove beta_*

--- voyages/apps/american/urls.py
+++ voyages/apps/american/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^essays', TemplateView.as_view(template_name='american/essays.html'), name='essays'),
     url(r'^downloads', TemplateView.as_view(template_name='american/downloads.html'), name='downloads'),
     url(r'^database', voyages.apps.american.views.index, name='database'), # url(r'^database', TemplateView.as_view(template_name='american/database.html'), name='database'),
-    url(r'^api/beta_ajax_search', voyages.apps.voyage.beta_views.ajax_search, name='beta_ajax_search'),
-    url(r'^api/beta_ajax_download', voyages.apps.voyage.beta_views.ajax_download, name='beta_ajax_download'),
-    url(r'^api', voyages.apps.voyage.beta_views.search_view, name='beta_search')
+    url(r'^api/search', voyages.apps.voyage.search_views.ajax_search, name='search'),
+    url(r'^api/download', voyages.apps.voyage.search_views.ajax_download, name='download'),
 ]
