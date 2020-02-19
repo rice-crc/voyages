4864d7558f6270a8bee687b746ea840bf667571d
Yang Li <yang@truman.edu>
2020-02-19 02:00:31 -0500

    up version to 2.2.9

--- voyages/__init__.py
+++ voyages/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = (2, 2, 8, None)
+__version_info__ = (2, 2, 9, None)
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join([str(i) for i in __version_info__[:-1]])
