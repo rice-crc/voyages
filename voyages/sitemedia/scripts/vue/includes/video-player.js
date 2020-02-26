ebc9b3ede438396047d0c65286a87fda205ea7b1
Yang Li <yang@truman.edu>
2020-02-26 08:43:22 -0500

    add timelapse intro video, new center, and style changes

--- voyages/sitemedia/scripts/vue/includes/video-player.js
+++ voyages/sitemedia/scripts/vue/includes/video-player.js
@@ -1,7 +1,28 @@
+// Site video
 $('#play-video').on('click', function (e) {
   e.preventDefault();
   $('#video-overlay').addClass('open');
   $("#video-overlay").append('<iframe src="https://player.vimeo.com/video/315957327?autoplay=1" frameborder="0" allow="autoplay; fullscreen" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe >');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function (e) {
+  e.preventDefault();
+  close_video();
+});
+
+$(document).keyup(function (e) {
+  if (e.keyCode === 27) { close_video(); }
+});
+
+function close_video() {
+  $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
+
+// Timelapse video
+$('#timelapse-video-btn').on('click', function (e) {
+  e.preventDefault();
+  $('#video-overlay').addClass('open');
+  $("#video-overlay").append('<iframe src="https://player.vimeo.com/video/390796627?autoplay=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>');
 });
 
 $('.video-overlay, .video-overlay-close').on('click', function (e) {
