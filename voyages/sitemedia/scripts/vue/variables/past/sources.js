d14932c62c237129404bd3c176ce8e0bc376ba04
Paulo Cesar <paulo@plabweb.com.br>
2020-07-05 19:36:23 -0300

    Integrate API search calls with frontend filters in Past

--- voyages/sitemedia/scripts/vue/variables/past/sources.js
+++ voyages/sitemedia/scripts/vue/variables/past/sources.js
@@ -1,30 +1,6 @@
-var_source_1 = new TextVariable({
-    varName: "source_1",
-    label: gettext("Source 1"),
-    description: "",
-  },{
-    op: "contains",
-    searchTerm: null,
-  },{
-    isImputed: false,
-    isAdvanced: false
-  });
-
-var_source_2 = new TextVariable({
-    varName: "source_2",
-    label: gettext("Source 2"),
-    description: "",
-  },{
-    op: "contains",
-    searchTerm: null,
-  },{
-    isImputed: false,
-    isAdvanced: false
-  });
-
-var_source_3 = new TextVariable({
-    varName: "source_3",
-    label: gettext("Source 3"),
+var_source = new TextVariable({
+    varName: "source",
+    label: gettext("Source"),
     description: "",
   },{
     op: "contains",
@@ -37,9 +13,7 @@ var_source_3 = new TextVariable({
 // all
 sources = {
   sources: {
-    var_source_1: var_source_1,
-    var_source_2: var_source_2,
-    var_source_3: var_source_3,
+    var_source: var_source,
 
     count: {
       changed: 0,
