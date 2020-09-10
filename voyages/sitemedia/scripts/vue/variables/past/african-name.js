a4493b5efb59977def31d68fc06c1887ea0ad227
Leonardo Barros <leonardo@plabweb.com.br>
2020-09-10 19:15:45 -0300

    Adding enslaved_id column and filter. Hidding modern country and ethnicity columns.

--- voyages/sitemedia/scripts/vue/variables/past/african-name.js
+++ voyages/sitemedia/scripts/vue/variables/past/african-name.js
@@ -1,3 +1,16 @@
+var_enslaved_id = new NumberVariable({
+    varName: "enslaved_id",
+    label: gettext("African ID"),
+    description: "",
+  },{
+    op: "is equal to",
+    searchTerm0: null,
+    searchTerm1: null
+  },{
+    isImputed: false,
+    isAdvanced: false
+  });
+
 var_searched_name = new TextVariable({
     varName: "searched_name",
     label: gettext("Name"),
@@ -27,6 +40,7 @@ africanName = {
   name: {
     var_searched_name: var_searched_name,
     var_exact_name_search: var_exact_name_search,
+    var_enslaved_id: var_enslaved_id,
 
     count: {
       changed: 0,
