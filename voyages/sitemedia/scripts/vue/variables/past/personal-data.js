d4aa84d83b8e329b9c09565a64b12d4a996be270
Leonardo Barros <leonardo@plabweb.com.br>
2020-09-11 17:47:00 -0300

    Adding height filter

--- voyages/sitemedia/scripts/vue/variables/past/personal-data.js
+++ voyages/sitemedia/scripts/vue/variables/past/personal-data.js
@@ -23,11 +23,25 @@ var_age_gender = new TreeselectVariable({
     isAdvanced: false,
   });
 
+var_height_range = new NumberVariable({
+    varName: "height_range",
+    label: gettext("Height"),
+    description: "",
+  },{
+    op: "is between",
+    searchTerm0: 12,
+    searchTerm1: 85
+  },{
+    isImputed: false,
+    isAdvanced: false
+  });
+
 // all
 personalData = {
   personalData: {
     var_age_range: var_age_range,
     var_age_gender: var_age_gender,
+    var_height_range: var_height_range,
 
     count: {
       changed: 0,
