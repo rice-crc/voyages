4ce287a9954cf1bdbc2989080d7a06f941b7437d
Leonardo Barros <leonardo@plabweb.com.br>
2020-12-01 14:25:29 -0300

    Adding forms to contribute page

--- voyages/apps/past/urls.py
+++ voyages/apps/past/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^api/ethnicities', voyages.apps.past.views.get_ethnicities, name='ethnicities'),
     url(r'^database', TemplateView.as_view(template_name='past/database.html'), name='database'),
     url(r'^contribute/(?P<id>.*)', TemplateView.as_view(template_name='past/contribute.html'), name='contribute'),
+    url(r'^enslaved_contribution', voyages.apps.past.views.enslaved_contribution),
     url(r'^store-audio/(?P<contrib_pk>[0-9]+)/(?P<name_pk>[0-9]+)/(?P<token>.*)', voyages.apps.past.views.store_audio)
     ]
--- voyages/sitemedia/scripts/vue/past-contribute/app.js
+++ voyages/sitemedia/scripts/vue/past-contribute/app.js
@@ -7,6 +7,21 @@ var pastContribute = new Vue({
         total_names: 0,
         recordings_content: '',
         audioList: {},
+        language_groups: new TreeselectVariable({
+            varName: "language_groups",
+            label: gettext("Language Group"),
+            description: "",
+        },{
+            op: "is one of",
+            searchTerm: [],
+        },{
+            isImputed: false,
+            isadvanced: false
+        }),
+        filterData: {
+          treeselectOptions: {
+          }
+        },
     },
     created: function() {
         var params = {
@@ -61,7 +76,70 @@ var pastContribute = new Vue({
     }
 });
 
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    mymap.fitBounds(e.target.getBounds());
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'transparent',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+var africaCountriesData = (function() {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': STATIC_URL+'maps/js/past/africa-hig.geo.json',
+        'dataType': "json",
+        'success': function(data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+var geojson;
+var mymap;
+
 $(function(){
+    $($('.review-recording').find('audio')[0]).on("canplay", function () {
+        $('.duration').html(secondsTimeSpanToMS(this.duration));
+    }).on('ended', function(){
+        $(this).siblings('.audio-controls').find('.play-button').removeClass('fas fa-stop').addClass('fas fa-play-circle');
+    });
+
     $('body').on('click', function (e) {
         //did not click a popover toggle, or icon in popover toggle, or popover
         if ($(e.target).parents('.popover').length === 0) {
@@ -111,4 +189,138 @@ $(function(){
             document.getElementById($(this).data('audio-id')).play();
         });
     });
+
+    $(".recording-button").click(function(){
+        start();
+        $(".recording-button").addClass('d-none');
+        $(".stop-recording-button").removeClass('d-none');
+    });
+
+    $(".stop-recording-button").click(function(){
+        stop();
+        $(".stop-recording-button").addClass('d-none');
+    });
+
+    $(".play-button").click(function(){
+        var audio = $(this).parent().siblings('audio')[0]
+
+        if ($(this).hasClass('fa-play-circle')) {
+            $(this).removeClass('fa-play-circle');
+            $(this).addClass('fa-stop');
+            audio.play();
+        } else {
+            $(this).removeClass('fa-stop');
+            $(this).addClass('fa-play-circle');
+            audio.pause();
+            audio.currentTime = 0;
+        }
+
+        // Prevent Default Action
+        return false;
+    });
+
+    mymap = L.map('mapid').setView([0.57128, 15.829786], 3);
+
+    $(".btn-next-section").click(function(){
+        $('#pills-profile-tab').tab('show');
+        return false;
+    });
+
+    $(".btn-previous-section").click(function(){
+        $('#pills-home-tab').tab('show');
+        return false;
+    });
+
+    $("#pills-profile-tab").on('shown.bs.tab', function(e) {
+      mymap.invalidateSize();
+    });
+
+    L.tileLayer('https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+        attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 0,
+        maxZoom: 22,
+        subdomains: 'abcd',
+        accessToken: 'NHr9dsp5NeloMikhmQAeJiSNxW2QDRz9cHNKGawiDlTdnFR2RvJBqEFbaxSjwRtY'
+    }).addTo(mymap);
+
+    geojson = L.geoJson(africaCountriesData, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(mymap);
+
 });
+
+const req = { audio: true, video: false };
+let stopAction = null;
+let chunks = [];
+let blobs = [];
+
+const soundClips = document.querySelector('.sound-clips');
+
+// THIS IS SAMPLE CODE TO TEST AUDIO RECORDINGS!
+function start() {
+    navigator.mediaDevices.getUserMedia(req)
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+            mediaRecorder.ondataavailable = function(e) {
+                chunks.push(e.data);
+            };
+            mediaRecorder.onstop = function(e) {
+                var reviewRecording = $('.review-recording');
+                var audio = document.getElementById('audio-recording-1');
+
+                const blob = new Blob(chunks, { 'type' : chunks[0].type });
+                chunks = [];
+                const audioURL = window.URL.createObjectURL(blob);
+                audio.src = audioURL;
+                audio.addEventListener("loadedmetadata", function () {
+                    if(this.duration === Infinity) {
+                        this.currentTime = 1e101;
+                        this.ontimeupdate = function() {
+                            this.ontimeupdate = () => {
+                                return;
+                            }
+                            $(this).siblings('.audio-controls').find('.duration').html(secondsTimeSpanToMS(this.duration));
+                            audio.currentTime = 0.1;
+                            audio.currentTime = 0;
+                        }
+                    } else {
+                        $(this).siblings('.audio-controls').find('.duration').html(secondsTimeSpanToMS(this.duration));
+                    }
+                });
+
+                audio.addEventListener('ended', function(){
+                    $(this).siblings('.audio-controls').find('.play-button').removeClass('fas fa-stop').addClass('fas fa-play-circle');
+                });
+
+                reviewRecording.find('.delete-button').on('click', function(){
+                    $(this).closest('.review-recording').addClass('d-none');
+                    $(this).closest('.review-recording').siblings('.recording-button').removeClass('d-none');
+                    return false;
+                });
+
+                reviewRecording.removeClass('d-none');
+            };
+
+            stopAction = function() {
+                var track = stream.getTracks()[0];
+                track.stop();
+                mediaRecorder.stop();
+            };
+        })
+        .catch(err => alert('Cannot start mic'));
+}
+
+function stop() {
+    if (stopAction) {
+        stopAction();
+    }
+}
+
+function secondsTimeSpanToMS(s) {
+    s = parseInt(s, 10);
+    var m = Math.floor(s/60); //Get remaining minutes
+    s -= m*60;
+    return (m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); //zero padding on minutes and seconds
+}
