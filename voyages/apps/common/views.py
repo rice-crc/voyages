rice-crc/voyages

commit d21f0bbe17a81a6febf75813095b8e925d60ee51

Adding enslaved filtered places API entrypoint. Refactoring common code with voyages search_views.get_filtered_results.

voyages/apps/common/views.py
@@ -6,6 +6,7 @@ from builtins import str
 import django
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
+from django.core.cache import cache
 from django.core.paginator import Paginator
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
@@ -27,6 +28,25 @@ def get_nations(_):
         } for n in Nationality.objects.all()
     }
     return JsonResponse(nations)
+
+
+def get_filtered_results(cache_key, place_pks_queryset):
+    result = cache.get(cache_key)
+    is_cached = result is not None
+    if not is_cached:
+        pks = list(place_pks_queryset)
+        place_query = Place.objects.filter(pk__in=pks)
+        result = get_ordered_places(place_query, False)
+        # Cache the data for 24h.
+        cache.set(cache_key, result, 24 * 60 * 60)
+    for d in result:
+        # Translate the corresponding entry.
+        geo_type = d['type']
+        d[geo_type] = _(d[geo_type])
+    return {
+        'is_cached': is_cached,
+        'data': result
+    }
 
 
 def get_ordered_places(place_query=None, translate=True):

voyages/apps/voyage/search_views.py
@@ -19,13 +19,13 @@ from django.views.decorators.http import require_POST
 from haystack.inputs import Raw
 from haystack.query import SearchQuerySet
 import unicodecsv
+from voyages.apps.common.views import get_filtered_results as common_get_filtered_results
 
 from voyages.apps.common.export import download_xls
 from voyages.apps.common.models import (SavedQuery,
                                         get_pks_from_haystack_results, get_values_from_haystack_results)
 from voyages.apps.common.views import \
     get_datatable_json_result as get_results_table
-from voyages.apps.common.views import get_ordered_places
 from voyages.apps.voyage.models import (Nationality, OwnerOutcome,
                                         ParticularOutcome, Place, Resistance,
                                         RigOfVessel, SlavesOutcome, TonType,
@@ -790,6 +790,7 @@ def get_var_options(request):
 
 
 @csrf_exempt
+@require_POST
 def get_filtered_places(request):
     """
     Obtains a list of places and corresponding regions/broad regions
@@ -799,27 +800,10 @@ def get_filtered_places(request):
     blank = '|blank|'
     var_name = data.get('var_name', blank)
     cache_key = '_filtered_places_' + var_name
-    result = cache.get(cache_key)
-    is_cached = result is not None
-    if not is_cached:
-        place_query = None
-        if var_name != blank:
-            pks = list(
-                VoyageItinerary.objects.values_list(var_name,
-                                                    flat=True).distinct())
-            place_query = Place.objects.filter(pk__in=pks)
-        result = get_ordered_places(place_query, False)
-        # Cache the data for 24h.
-        cache.set(cache_key, result, 24 * 60 * 60)
-    for d in result:
-        # Translate the corresponding entry.
-        geo_type = d['type']
-        d[geo_type] = _(d[geo_type])
-    return JsonResponse({
-        'filtered_var_name': var_name if var_name != blank else 'None',
-        'is_cached': is_cached,
-        'data': result
-    })
+    filtered = common_get_filtered_results(cache_key,
+        VoyageItinerary.objects.values_list(var_name, flat=True).distinct())
+    filtered['filtered_var_name'] = var_name if var_name != blank else 'None'
+    return JsonResponse(filtered)
 
 
 @csrf_exempt

voyages/apps/past/views.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, unicode_literals
 
-import itertools
 import json
 import uuid
 from builtins import str
@@ -18,6 +17,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from past.utils import old_div
 
+from voyages.apps.common.views import get_filtered_results
 from .models import (AltLanguageGroupName, Enslaved,
                      EnslavedContribution, EnslavedContributionLanguageEntry,
                      EnslavedContributionNameEntry, EnslavedSearch,
@@ -48,6 +48,29 @@ def _generate_table(query, table_params, data_adapter=None):
             page = changed
     response_data['data'] = list(page)
     return response_data
+
+
+@csrf_exempt
+@require_POST
+def get_enslaved_filtered_places(request):
+    """
+    Obtains a list of places and corresponding regions/broad regions that are
+    present in a given field of VoyageItinerary when filtered to the enslaved of
+    the given dataset.
+    """
+    data = json.loads(request.body)
+    var_name = data.get('var_name')
+    dataset = data.get('dataset')
+    if var_name is None or dataset is None:
+        return JsonResponse({ "error": "Both dataset and var_name must be set" })
+    cache_key = '_filtered_places_ENSLAVED_' + str(dataset) + "_" + var_name
+    filtered = get_filtered_results(cache_key, Enslaved.objects.filter(dataset=dataset).
+        select_related('voyage__voyage_itinerary__' + var_name).
+        values_list('voyage__voyage_itinerary__' + var_name + '_id', flat=True).
+        distinct())
+    filtered['filtered_var_name'] = var_name
+    filtered['dataset'] = dataset
+    return JsonResponse(filtered)
 
 
 @csrf_exempt

voyages/apps/past/urls.py
@@ -32,5 +32,8 @@ urlpatterns = [
         voyages.apps.past.views.enslaved_contribution),
     url(r'^store-audio/(?P<contrib_pk>[0-9]+)/'
         r'(?P<name_pk>[0-9]+)/(?P<token>.*)',
-        voyages.apps.past.views.store_audio)
+        voyages.apps.past.views.store_audio),
+    url(r'enslaved-filtered-places',
+        voyages.apps.past.views.get_enslaved_filtered_places,
+        name='enlsaved-filtered-places')
 ]

commit 4d13c5e4f66d4a5141993cf57b2172c73eb0d8dd

Fixing bug introduced in d21f0bbe17a81a6febf75813095b8e925d60ee51 when search_views.get_filtered_places is called without a var_name.

voyages/apps/common/views.py
@@ -34,8 +34,10 @@ def get_filtered_results(cache_key, place_pks_queryset):
     result = cache.get(cache_key)
     is_cached = result is not None
     if not is_cached:
-        pks = list(place_pks_queryset)
-        place_query = Place.objects.filter(pk__in=pks)
+        place_query = None
+        if place_pks_queryset:
+            pks = list(place_pks_queryset)
+            place_query = Place.objects.filter(pk__in=pks)
         result = get_ordered_places(place_query, False)
         # Cache the data for 24h.
         cache.set(cache_key, result, 24 * 60 * 60)

voyages/apps/voyage/search_views.py
@@ -800,8 +800,8 @@ def get_filtered_places(request):
     blank = '|blank|'
     var_name = data.get('var_name', blank)
     cache_key = '_filtered_places_' + var_name
-    filtered = common_get_filtered_results(cache_key,
-        VoyageItinerary.objects.values_list(var_name, flat=True).distinct())
+    qs = VoyageItinerary.objects.values_list(var_name, flat=True).distinct() if var_name != blank else None
+    filtered = common_get_filtered_results(cache_key, qs)
     filtered['filtered_var_name'] = var_name if var_name != blank else 'None'
     return JsonResponse(filtered)
 