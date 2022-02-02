c698755874df2ab783e80e6e73741d8cd25451c8
Domingos <domingos@dotproduct.com.br>
2022-02-02 16:04:26 -0300

    Adding back js files needed for TinyMCE editor initialization.

--- voyages/apps/past/urls.py
+++ voyages/apps/past/urls.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.views.generic import RedirectView
 from django.views.generic import TemplateView
 
 import voyages.apps.past.views
