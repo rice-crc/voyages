c0733cf88cf61b1b73d51ec4c50b598ed1763973
Derek Keller <derekjkeller@users.noreply.github.com>
2022-04-22 15:00:14 -0500

    Updates settings.py for template debug

--- voyages/settings.py
+++ voyages/settings.py
@@ -5,7 +5,7 @@ import sys
 
 from django.utils.translation import ugettext_lazy as _
 
-TEMPLATE_DEBUG  = True
+DEBUG  = True
 
 # Django settings for voyages project.
 
@@ -112,7 +112,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
             ],
             'string_if_invalid': 'Nothing',
-            'debug': True,
+            'debug': DEBUG,
         },
     },
 ]
