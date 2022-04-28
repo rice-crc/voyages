2d7f5d4e34ec98f83ba779993b51e9c7d94d49ca
wagnerps <wagnerps@gmail.com>
2022-04-28 17:40:37 -0300

    Blog - reusable carousel component - limit number of slides.

--- voyages/apps/blog/templatetags/blog_carousel.py
+++ voyages/apps/blog/templatetags/blog_carousel.py
@@ -9,5 +9,5 @@ def show_last_posts(context):
     request = context['request']
     lang_code = request.LANGUAGE_CODE or "en"        
     
-    return {"posts" : Post.objects.filter(status=PUBLISH_STATUS, language=lang_code).order_by('-created_on').exclude(tags__in = Tag.objects.filter(slug__in = ['author-profile','institution-profile']) )}
+    return {"posts" : Post.objects.filter(status=PUBLISH_STATUS, language=lang_code).order_by('-created_on').exclude(tags__in = Tag.objects.filter(slug__in = ['author-profile','institution-profile']) )[:9]}
     
