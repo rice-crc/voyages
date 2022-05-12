a1daa6392961a72e19387f98e7f476adad4978c5
wagnerps <wagnerps@gmail.com>
2022-05-12 16:48:43 -0300

    Carousel - added CAROUSEL_MAX_POSTS setting

--- voyages/apps/blog/templatetags/blog_carousel.py
+++ voyages/apps/blog/templatetags/blog_carousel.py
@@ -5,19 +5,18 @@ from ..models import PUBLISH_STATUS, Post, Tag
 register = template.Library()
 
 @register.inclusion_tag('blog/carousel.html', takes_context=True)
-def show_last_posts(context, maxresults = 9):
+def show_last_posts(context, max_results = None):
     request = context['request']
     lang_code = request.LANGUAGE_CODE or "en"        
-
-    print("max results")
-    print(maxresults)
-
-    print('context here')
-    print(context)
-
     
+    if max_results is None:
+        try:
+            from voyages.localsettings import CAROUSEL_MAX_POSTS
+            max_results = CAROUSEL_MAX_POSTS
+        except Exception as e:
+            max_results = 9
     
     STATIC_URL = context['STATIC_URL']
     
-    return {"STATIC_URL":STATIC_URL,"posts" : Post.objects.filter(status=PUBLISH_STATUS, language=lang_code, tags__slug__in=['news']).order_by('-created_on').exclude(tags__in = Tag.objects.filter(slug__in = ['author-profile','institution-profile']) )[:maxresults]}
+    return {"STATIC_URL":STATIC_URL,"posts" : Post.objects.filter(status=PUBLISH_STATUS, language=lang_code, tags__slug__in=['news']).order_by('-created_on').exclude(tags__in = Tag.objects.filter(slug__in = ['author-profile','institution-profile']) )[:max_results]}
     
