d9cef32d58d835e3f6ba3d455b3ac3a71ea32057
Domingos <domingos@dotproduct.com.br>
2022-07-21 20:45:39 -0300

    Resetting pagination after search changes without clearing all DataTable state. This allow toggled columns to remain after search changes.

--- voyages/sitemedia/scripts/vue/enslavers/app.js
+++ voyages/sitemedia/scripts/vue/enslavers/app.js
@@ -1,3 +1,7 @@
+function resetPagination(datatable) {
+  datatable.page('first').draw(false);
+}
+
 // main app
 var searchBar = new Vue({
   el: "#search-bar",
@@ -453,15 +457,15 @@ var searchBar = new Vue({
       // var searchTerms = searchAll(this.filter, this.filterData);
       // alert(JSON.stringify(searchTerms));
       // search(this.searchFilter, searchTerms);
-      $("#results_main_table").DataTable().state.clear();
       this.refresh();
+      resetPagination($("#results_main_table").DataTable());
     },
 
     // reset inputs, filters, and counts back to default state
     reset(group, subGroup) {
-      $("#results_main_table").DataTable().state.clear();
       resetFilter(this.filter, group, subGroup);
       this.refresh();
+      resetPagination($("#results_main_table").DataTable());
     },
 
     clearFilter(filter) {
@@ -477,9 +481,9 @@ var searchBar = new Vue({
     },
 
     resetAll() {
-      $("#results_main_table").DataTable().state.clear();
       this.refreshPage();
       this.resetURL();
+      resetPagination($("#results_main_table").DataTable());
     },
 
     refresh() {
