rice-crc/voyages

commit 08cef523baace8a3ef6e47c301fca1f4fd9aeeb8

Using URL routing for editor_main in editorial platform.

voyages/apps/contribute/urls.py
@@ -49,7 +49,7 @@ urlpatterns = [
     url(r'edit_voyage', views.edit, name='edit_voyage'),
     url(r'merge_voyages', views.merge, name='merge_voyages'),
     url(r'new_voyage', views.new_voyage, name='new_voyage'),
-    url(r'editor_main', views.editor_main, name='editor_main'),
+    url(r'editor_main/?(?P<active_tab>\w*)$', views.editor_main, name='editor_main'),
     url(r'review_request/(?P<review_request_id>\d+)',
         views.review_request,
         name='review_request'),

voyages/apps/contribute/views.py
@@ -972,10 +972,10 @@ def voyage_to_dict(voyage):
 
 
 @login_required()
-def editor_main(request):
+def editor_main(request, active_tab):
     if not request.user.is_superuser:
         return HttpResponseForbidden()
-    return render(request, 'contribute/editor_main.html')
+    return render(request, 'contribute/editor_main.html', { "active_tab": active_tab or "requests" })
 
 
 def get_reviews_by_status(statuses, display_interim_data=False):

voyages/apps/contribute/templates/contribute/editor_main.html
@@ -35,18 +35,18 @@
 
   <!-- Nav tabs -->
   <ul class="nav nav-pills nav-fill" role="tablist">
-    <li class="nav-item" role="presentation"><a class="nav-link " id="tab_pending" href="#pending" aria-controls="voyages" role="tab" data-toggle="tab">{% trans "Voyages" %}</a></li>
-    <li class="nav-item" role="presentation"><a class="nav-link active" id="tab_requests" href="#requests" aria-controls="requests" role="tab" data-toggle="tab">{% trans "Requests" %}</a></li>
-    <li class="nav-item" role="presentation"><a class="nav-link" id="tab_enslavers_contrib" href="#enslavers_contrib" aria-controls="requests" role="tab" data-toggle="tab">{% trans "Enslavers" %}</a></li>
-    <li class="nav-item" role="presentation"><a class="nav-link " href="#users" aria-controls="users" role="tab" data-toggle="tab">{% trans "Users" %}</a></li>
-    <li class="nav-item" role="presentation"><a class="nav-link " href="#sources" aria-controls="sources" role="tab" data-toggle="tab">{% trans "Source Codes" %}</a></li>
-    <li class="nav-item" role="presentation"><a class="nav-link " href="#publish" aria-controls="publish" role="tab" data-toggle="tab">{% trans "Publish New DB Version" %}</a></li>
-    <li class="nav-item" role="presentation"><a class="nav-link " href="#downloads" aria-controls="downloads" role="tab" data-toggle="tab">{% trans "Download Voyages" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'pending' %} active{% endif %}" id="tab_pending" href="#pending" aria-controls="voyages" role="tab" data-toggle="tab">{% trans "Voyages" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'requests' %} active{% endif %}" id="tab_requests" href="#requests" aria-controls="requests" role="tab" data-toggle="tab">{% trans "Requests" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'enslavers_contrib' %} active{% endif %}" id="tab_enslavers_contrib" href="#enslavers_contrib" aria-controls="requests" role="tab" data-toggle="tab">{% trans "Enslavers" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'users' %} active{% endif %}" href="#users" aria-controls="users" role="tab" data-toggle="tab">{% trans "Users" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'sources' %} active{% endif %}" href="#sources" aria-controls="sources" role="tab" data-toggle="tab">{% trans "Source Codes" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'publish' %} active{% endif %}" href="#publish" aria-controls="publish" role="tab" data-toggle="tab">{% trans "Publish New DB Version" %}</a></li>
+    <li class="nav-item" role="presentation"><a class="nav-link{% if active_tab == 'downloads' %} active{% endif %}" href="#downloads" aria-controls="downloads" role="tab" data-toggle="tab">{% trans "Download Voyages" %}</a></li>
   </ul>
 
   <!-- Tab panes -->
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane" id="pending">
+    <div role="tabpanel" class="tab-pane {% if active_tab == 'pending' %} active{% endif %}" id="pending">
       <div class="page-title-1">
         Edit Voyages
       </div>
@@ -72,7 +72,7 @@
         </table>
       </div>
     </div>
-    <div role="tabpanel" class="tab-pane active" id="requests">
+    <div role="tabpanel" class="tab-pane{% if active_tab == 'requests' %} active{% endif %}" id="requests">
       <div class="page-title-1">
         Edit Requests
       </div>
@@ -96,7 +96,7 @@
         </table>
       </div>
     </div>
-    <div role="tabpanel" class="tab-pane" id="enslavers_contrib">
+    <div role="tabpanel" class="tab-pane{% if active_tab == 'enslavers_contrib' %} active{% endif %}" id="enslavers_contrib">
         <table id="enslavers_table" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
             <tr>
@@ -110,7 +110,7 @@
             </tbody>
         </table>
     </div>
-    <div role="tabpanel" class="tab-pane" id="users">
+    <div role="tabpanel" class="tab-pane{% if active_tab == 'users' %} active{% endif %}" id="users">
       <div class="page-title-1">
         Edit Users
       </div>
@@ -128,10 +128,10 @@
         <a class="btn btn-solid" href="/admin/voyage/voyagesources/">{% trans "Sources on Live Admin" %}</a>
       </div>
     </div>
-    <div role="tabpanel" class="tab-pane" id="publish">
+    <div role="tabpanel" class="tab-pane{% if active_tab == 'publish' %} active{% endif %}" id="publish">
         {% include "contribute/publication_partial.html" %}
     </div>
-    <div role="tabpanel" class="tab-pane" id="downloads">
+    <div role="tabpanel" class="tab-pane{% if active_tab == 'downloads' %} active{% endif %}" id="downloads">
         {% include "contribute/downloads_partial.html" %}
     </div>
   </div>
@@ -298,7 +298,7 @@
                         },
                         render: function(data, type) {
                             if (type === 'display') {
-                                return `<a href="/contribute/enslaver_contribution_review/${data.pk}"><i class="fa fa-${data.icon}"></i></a>`;
+                                return `<a href="/contribute/enslaver_contribution_review/${data.pk}"><span class="fa fa-${data.icon}"></span></a>`;
                             }
                             return data;
                         }
@@ -323,6 +323,7 @@
         } else if (href == '#enslavers_contrib') {
             loadEnslaversContribList();
         }
+        history.pushState("", "", `/contribute/editor_main/${href.substring(1)}`);
     };
     loadTabData('#' + $('.tab-pane.active').attr('id'));
 

commit d1016c1372d8d0472a026e86fc5125c12891a0e2

Initial inclusion of (biographical) sources in enslaver contrib. Fix a URL path bug introduced by previous commit 08cef523baace8a3ef6e47c301fca1f4fd9aeeb8.

voyages/apps/contribute/urls.py
@@ -49,7 +49,8 @@ urlpatterns = [
     url(r'edit_voyage', views.edit, name='edit_voyage'),
     url(r'merge_voyages', views.merge, name='merge_voyages'),
     url(r'new_voyage', views.new_voyage, name='new_voyage'),
-    url(r'editor_main/?(?P<active_tab>\w*)$', views.editor_main, name='editor_main'),
+    url(r'editor_main/(?P<active_tab>\w*)$', views.editor_main),
+    url(r'editor_main', views.editor_main, name='editor_main'),
     url(r'review_request/(?P<review_request_id>\d+)',
         views.review_request,
         name='review_request'),

voyages/apps/contribute/views.py
@@ -53,7 +53,7 @@ from voyages.apps.contribute.publication import (
     export_contributions, export_from_voyages, full_contribution_id,
     get_csv_writer, get_filtered_contributions, get_header_csv_text,
     publish_accepted_contributions, safe_writerow)
-from voyages.apps.past.models import Enslaved, EnslavedContribution, EnslavedContributionLanguageEntry, EnslavedContributionNameEntry, EnslavedContributionStatus, EnslavementRelation, EnslaverAlias, EnslaverCachedProperties, EnslaverContribution, EnslaverIdentity, EnslaverInRelation, EnslaverVoyageConnection, LanguageGroup, ModernCountry
+from voyages.apps.past.models import Enslaved, EnslavedContribution, EnslavedContributionLanguageEntry, EnslavedContributionNameEntry, EnslavedContributionStatus, EnslavementRelation, EnslaverAlias, EnslaverCachedProperties, EnslaverContribution, EnslaverIdentity, EnslaverInRelation, EnslaverVoyageConnection, LanguageGroup, ModernCountry, EnslaverIdentitySourceConnection
 from voyages.apps.past.views import _get_audio_filename
 from voyages.apps.voyage.cache import VoyageCache
 from voyages.apps.voyage.forms import VoyagesSourcesAdminForm
@@ -972,7 +972,7 @@ def voyage_to_dict(voyage):
 
 
 @login_required()
-def editor_main(request, active_tab):
+def editor_main(request, active_tab=None):
     if not request.user.is_superuser:
         return HttpResponseForbidden()
     return render(request, 'contribute/editor_main.html', { "active_tab": active_tab or "requests" })
@@ -2460,6 +2460,10 @@ def init_enslaver_interim(request):
         eid = enslaver.pop('id')
         edata = { 'id': eid, 'personal_data': enslaver }
         edata['aliases'] = { a.id: _get_alias_data(a) for a in EnslaverAlias.objects.filter(identity=eid) }
+        edata['sources'] = { s['pk']: s for s in EnslaverIdentitySourceConnection.objects \
+            .filter(identity_id=eid) \
+            .values('pk', 'source_id', 'source_order', 'text_ref', full_ref=F('source__full_ref'))
+        }
         return edata
 
     identities = { e['id']: e for e in [_get_enslaver_data(enslaver) for enslaver in originals] }
@@ -2597,6 +2601,8 @@ def _create_enslaver_update_actions(contrib, check_transaction_tags=None):
     current_relations = {int(r['relation_id']): r for r in EnslaverInRelation.objects \
         .filter(enslaver_alias_id__in=current_aliases.keys()) \
         .values('enslaver_alias_id', 'relation_id')}
+    current_sources = {int(s.pk): s for s in EnslaverIdentitySourceConnection.objects \
+        .filter(identity_id__in=[k for k in identities.keys() if _isint(k)])}
     proposed_voyage_conn = {}
     proposed_relations = {}
     for si in saved_identities:
@@ -2649,6 +2655,39 @@ def _create_enslaver_update_actions(contrib, check_transaction_tags=None):
             for r in a['relations']:
                 rid = int(r['pk'])
                 proposed_relations[rid] = {'relation_id': rid, 'enslaver_alias_id': tag}
+        # Now handle source connections (these are directly attached to the
+        # EnslaverIdentity).
+        for source in si.get('sources', {}).items():
+            if not _isint(source['source_id']):
+                # This is a newly created source.
+                actions.append({
+                    'description': u_('Creating a new biographical source'),
+                    'action': 'new',
+                    'model': VoyageSources.__name__,
+                    'data': { k: source[k] for k in ['short_ref', 'full_ref', 'source_type'] },
+                    'tag': f"{VoyageSources.__name__}__{source['source_id']}"
+                })
+            if not source['pk'] in current_sources:
+                # Create new source connection.
+                # Note: we do not support updating a source connection (the
+                # contributor/editor can always delete and create a new one if
+                # needed).
+                actions.append({
+                    'description': u_('Creating an enslaver identity <-> biographical source connection'),
+                    'action': 'new',
+                    'model': EnslaverIdentitySourceConnection.__name__,
+                    'data': source
+                })
+            else:
+                current_sources.pop(source['pk'])
+    # Delete any EnslaverIdentitySourceConnection that still remains.
+    for s in current_sources.values():
+        actions.append({
+            'description': 'Removing enslaver identity <-> bibliographical source connection',
+            'action': 'delete',
+            'model': EnslaverIdentitySourceConnection.__name__,
+            'match': { 'identity_id': s.identity_id, 'text_ref': s.text_ref }
+        })
     # Compute voyage connection delta.
     exact_vconn_matches = set()
     for key, rel in current_voyage_conn.items():

voyages/apps/past/templates/past-contribute/_enslaver_contrib_aliases.html
@@ -36,7 +36,7 @@
         </div>
         <div>
             <select v-model="selectedAlias" id="aliases-select" name="aliases" size="6"
-                class="form-control enslaver_contrib_fullspan_control ">
+                class="form-control enslaver_contrib_fullspan_control">
                 <option v-for="alias in selectedIdentity.aliases" class="list-group-item"
                     v-bind:class="{ bold: alias.name === selectedIdentity.personal_data.principal_alias }"
                     v-bind:value="alias.id">[[ alias.name ]] (id [[ alias.id ]])</option>

voyages/apps/past/templates/past-contribute/_enslaver_contrib_sources.html
@@ -1,0 +1,26 @@
+{% load i18n %}
+
+<div style="max-height: calc(100vh - 150px); overflow-x: clip; overflow-y: none;">
+    <!-- For split contributions, the user needs to pick the identity to manage
+    source references. -->
+    <div class="row" v-if="interim.type === 'split'">
+        <div class="col-md-6 form-inline">
+            <label for="identities">{% trans "Identity:" %}</label>
+            <select v-model="selectedIdentity" name="identities" class="form-control">
+                <option v-for="identity in interim.identities" v-bind:value="identity">
+                    [[identity.personal_data.principal_alias]]
+                </option>
+            </select>
+        </div>
+    </div>
+    <div class="btn-group" role="group" aria-label="Sources actions">
+        <button type="button" v-on:click="delSourceConn" v-bind:class="{ disabled: !selectedSource }"
+            class="btn btn-danger">{% trans "Delete Source Reference" %}
+        </button>
+    </div>
+    <select size="6" class="form-control enslaver_contrib_fullspan_control" multiple="false" id="sources-select" name="sources" v-model="selectedSource">
+        <option class="list-group-item" v-for="source in selectedIdentity.sources" v-bind:value="[[ source.pk ]]">
+            [[ source.text_ref ]] - [[ source.full_ref ]]
+        </option>
+    </select>
+</div>

voyages/apps/past/templates/past/enslavers_contribute.html
@@ -58,6 +58,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#personal" data-toggle="tab" data-target="#personal">{% trans 'Personal information' %}</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#sources" data-toggle="tab" data-target="#sources">{% trans 'Biographical Sources' %}</a>
+                </li>
                 {% endif %}
                 <li class="nav-item">
                     <a 
@@ -90,6 +93,9 @@
                 <div id="personal" class="tab-pane fade">
                     {% include "past-contribute/_enslaver_contrib_personal.html" %}
                 </div>
+                <div id="sources" class="tab-pane fade">
+                    {% include "past-contribute/_enslaver_contrib_sources.html" %}
+                </div>
                 {% endif %}
                 <div
                     {% if mode != 'delete' %}
@@ -99,9 +105,6 @@
                     {% endif %}
                         id="notes">
                     <textarea name="notes" class="form-control enslaver_contrib_fullspan_control" v-model="interim.notes"></textarea>
-                    {% if mode == 'delete' %}
-                        
-                    {% endif %}
                 </div>
                 <div id="review" class="tab-pane fade">
                     <div class="panel panel-danger" v-if="validation.length > 0">
@@ -243,6 +246,7 @@
                     ? interim.identities["merged"]
                     : interim.identities[Object.keys(interim.identities)[0]],
                 selectAliasChildren: [],
+                selectedSource: null,
                 linkVoyageInput: null,
                 linkVoyageRole: null,
                 createAliasInput: null,
@@ -337,6 +341,13 @@
                     delete this.selectedIdentity.aliases[this.selectedAlias];
                     this.selectedAlias = null;
                     this.contribActions = null;
+                },
+                delSourceConn: function(event) {
+                    if (!!this.selectedIdentity && !!this.selectedSource) {
+                        delete this.selectedIdentity.sources[this.selectedSource]
+                        this.selectedSource = null;
+                        this.contribActions = null;
+                    }
                 },
                 moveAlias: function(event) {
                     const target = this.interim.identities[event.target.getAttribute("value")];