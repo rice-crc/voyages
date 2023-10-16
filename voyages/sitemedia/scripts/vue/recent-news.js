d355c4ed856eb482b14d8ee4d97e3f92259f1978
John Connor <jcm10@rice.edu>
2023-10-16 10:04:05 -0500

    commented out buggy news carousel js

--- voyages/sitemedia/scripts/vue/recent-news.js
+++ voyages/sitemedia/scripts/vue/recent-news.js
@@ -1,5 +1,5 @@
 var recentNews = new Vue({
-  el: "#recent-news",
+//   el: "#recent-news",
   delimiters: ['[[', ']]'],
   data: {
     news: []
@@ -32,20 +32,23 @@ var recentNews = new Vue({
       vm.response = response.data;
 
       articles = [...response.data.items].reverse().slice(0, articleMaxNum);
-
+//       console.log('ARTICLES',articles)
       var article = [];
       articles.forEach((article, i) => {
         article.url = article.url.replace(/^http:\/\//i, '//');
 
         axios.get(article.url)
         .then(function (articleResponse) {
+//          console.log("ARTICLE RESPONSE",articleResponse.data)
           var el = $("<div></div>");
           el.html(articleResponse.data);
-
+//           console.log("EL",el)
+//           console.log($(".page-title-1", el).innerText)
+//           console.log(el)
           article.id = i;
-          article.title = $(".page-title-1", el)[0].innerText;
-          article.timestamp = $(".method-date", el)[0].innerText;
-          article.text = $("p", el)[0].innerText.substring(0, 200) + "...";
+//           article.title = $(".page-title-1", el).innerText;
+//           article.timestamp = $(".method-date", el).innerText;
+//           article.text = $("p", el)[0].innerText.substring(0, 200) + "...";
           article.url = article.url.replace(
             "common/getflatpage/about/",
             "about/about#"
